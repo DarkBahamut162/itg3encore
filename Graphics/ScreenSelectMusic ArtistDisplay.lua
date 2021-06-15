046de38bcef2e542016cf626a52deb6327d03ac1
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-06-15 15:29:08 +0200

    Fix course stuff, comments & ScreenOptions

--- Graphics/ScreenSelectMusic ArtistDisplay.lua
+++ Graphics/ScreenSelectMusic ArtistDisplay.lua
@@ -1,6 +1,6 @@
 return Def.ActorFrame{
 	LoadFont("_r bold shadow 30px")..{
-		InitCommand=function(self) self:zoom(0.66):maxwidth(360):halign(0) end;
+		InitCommand=function(self) self:zoom(0.66):maxwidth(350):halign(0) end;
 		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end;
 		CurrentCourseChangedMessageCommand=function(self) self:playcommand("Set") end;
 		CurrentTrailP1ChangedMessageCommand=function(self) self:playcommand("Set") end;
@@ -16,9 +16,13 @@ return Def.ActorFrame{
 				if trail then
 					local artists = trail:GetArtists()
 					for i=1,#artists do
-						text = text .." ".. artists[i]
-						if i < #artists then
-							text = text ..","
+						--prevent duplicates ~DarkBahamut162
+						if not string.find(text,artists[i]) then
+							if i == 1 then
+								text = artists[i]
+							elseif i < #artists then
+								text = text .. ", " .. artists[i]
+							end
 						end
 					end
 				else
