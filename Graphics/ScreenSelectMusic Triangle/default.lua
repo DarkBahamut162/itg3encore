beac8ec062c893c844fc28f76ec86bef77e2e2c9
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-08-17 18:56:00 +0200

    Re-Add full effect on wheel cursor

--- Graphics/ScreenSelectMusic Triangle/default.lua
+++ Graphics/ScreenSelectMusic Triangle/default.lua
@@ -1,4 +1,10 @@
 local t = Def.ActorFrame{
+	LoadActor("wheel cursor glow")..{
+		InitCommand=function(self) self:diffusealpha(0) end;
+		OnCommand=function(self) self:sleep(0.1):linear(0.3):diffusealpha(1) end;
+		OffCommand=function(self) self:stoptweening():decelerate(0.3):diffusealpha(0) end;
+		CurrentSongChangedMessageCommand=function(self) self:stoptweening():linear(0.15):x(10):diffusealpha(0):decelerate(0.3):diffusealpha(1):x(0) end;
+	};
 	LoadActor("glow")..{
 		InitCommand=function(self) self:diffusealpha(0) end;
 		OnCommand=function(self) self:sleep(0.1):linear(0.3):diffusealpha(1):diffuseshift():effectcolor1(color("#00FFFF")):effectcolor2(color("#0000CC")):effectclock("beat") end;
@@ -11,20 +17,17 @@ local t = Def.ActorFrame{
 		OffCommand=function(self) self:stoptweening():decelerate(0.3):diffusealpha(0) end;
 		CurrentSongChangedMessageCommand=function(self) self:stoptweening():linear(0.15):x(10):decelerate(0.3):x(0) end;
 	};
+	LoadActor("blueflare")..{
+		InitCommand=function(self) self:diffusealpha(0):blend(Blend.Add) end;
+		OnCommand=function(self) self:playcommand("Refresh") end;
+		RefreshCommand=function(self) self:stoptweening(1):zoom(0):diffusealpha(1):sleep(0.6):accelerate(0.5):zoom(5):diffusealpha(0) end;
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Refresh") end;
+	};
 	LoadActor("outline")..{
 		InitCommand=function(self) self:diffusealpha(0):blend(Blend.Add) end;
 		OffCommand=function(self) self:stoptweening():decelerate(0.3):diffusealpha(0) end;
-		-- original version
 		RefreshCommand=function(self) self:stoptweening():linear(0.1):diffusealpha(0):sleep(0.2):diffusealpha(1):cropright(-0.3):cropleft(1.1):fadeleft(0.05):faderight(0.05):diffusealpha(1):linear(0.72):cropright(1):cropleft(-0.3) end;
 		CurrentSongChangedMessageCommand=function(self) self:diffusealpha(0):stoptweening():playcommand("Refresh") end;
-
-		-- freem's version
-		--[[
-		NextSongMessageCommand=function(self) self:diffusealpha(0):stoptweening():playcommand("GoRight") end;
-		PreviousSongMessageCommand=function(self) self:diffusealpha(0):stoptweening():playcommand("GoLeft") end;
-		GoLeftCommand=function(self) self:stoptweening():linear(0.1):diffusealpha(0):sleep(0.2):diffusealpha(1):cropright(-0.3):cropleft(1.1):fadeleft(0.05):faderight(0.05):diffusealpha(1):linear(0.72):cropright(1):cropleft(-0.3) end;
-		GoRightCommand=function(self) self:stoptweening():linear(0.1):diffusealpha(0):sleep(0.2):diffusealpha(1):cropleft(-0.3):cropright(1.1):faderight(0.05):fadeleft(0.05):diffusealpha(1):linear(0.72):cropleft(1):cropright(-0.3) end;
-		--]]
 	};
 };
 
