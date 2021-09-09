28d309697c43ed2c64025d6508abbd20279cd772
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-09-09 12:56:36 +0200

    Updated OutFox graphics

--- BGAnimations/ScreenOITG overlay/sm5/default.lua
+++ BGAnimations/ScreenOITG overlay/sm5/default.lua
@@ -1,25 +1,25 @@
 local t = Def.ActorFrame{
 	InitCommand=function(self) self:fov(70) end;
-	LoadActor("_arrow")..{
-		InitCommand=function(self) self:x(-230+50) end;
-		OnCommand=function(self) self:wag():effectmagnitude(0,0,16):effectperiod(2.5):zoom(0.6) end;
+	LoadActor("_logo")..{
+		InitCommand=function(self) self:x(-180) end;
+		OnCommand=function(self) self:pulse():effectclock("beat"):effectperiod(0.83):effectmagnitude(1,1.25,0):zoom(0.6) end;
 	};
 	LoadActor("_text")..{
-		OnCommand=function(self) self:x(40):zoom(0.4) end;
+		OnCommand=function(self) self:x(120):zoom(0.6) end;
 	};
 	LoadActor("_text")..{
 		Name="TextGlow";
 		InitCommand=function(self) self:blend(Blend.Add):diffusealpha(0.05) end;
-		OnCommand=function(self) self:x(40):glowshift():effectperiod(2.5):effectcolor1(color("1,1,1,0.25")):effectcolor2(color("1,1,1,1")):zoom(0.4) end;
+		OnCommand=function(self) self:x(120):glowshift():effectperiod(2.5):effectcolor1(color("1,1,1,0.25")):effectcolor2(color("1,1,1,1")):zoom(0.6) end;
 	};
 	LoadFont("_v profile")..{
 		Text="Powered by";
-		InitCommand=function(self) self:x(-140+25):y(-36-30):halign(0):shadowlength(2) end;
+		InitCommand=function(self) self:x(-101):y(-69):halign(0):shadowlength(2) end;
 		OnCommand=function(self) self:diffusealpha(0):sleep(1):linear(0.5):diffusealpha(1) end;
 	};
 	LoadFont("_v 26px bold shadow")..{
-		Text="https://projectmoon.dance/";
-		InitCommand=function(self) self:x(36):y(55+10):shadowlength(2):zoom(0.75) end;
+		Text="https://projectoutfox.com/";
+		InitCommand=function(self) self:x(23):y(69):shadowlength(2):zoom(0.75) end;
 		OnCommand=function(self) self:diffusealpha(0):sleep(1):linear(0.5):diffusealpha(1) end;
 	};
 };
