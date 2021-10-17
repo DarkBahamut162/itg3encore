81edeb0a322466de946225ab45ccc2dd5785df62
DarkBahamut162 <dark_bahamut162@hotmail.com>
2021-10-17 08:53:20 +0200

    Fixed ScreenTestInput

---
+++ BGAnimations/ScreenTestInput underlay.lua
@@ -0,0 +1,19 @@
+return Def.ActorFrame {
+	LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_sides"));
+	LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_base"));
+	LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_normaltop"));
+
+	Def.DeviceList {
+		Font="Common Normal",
+		InitCommand= function (self)
+			self:x(SCREEN_CENTER_X+280):y(SCREEN_BOTTOM-102):zoom(0.4):halign(1):valign(0):maxwidth(400)
+		end
+	};
+
+	Def.InputList {
+		Font="Common Normal",
+		InitCommand=function (self)
+			self:x(SCREEN_LEFT+145):y(SCREEN_CENTER_Y):zoom(1):halign(0):vertspacing(8):maxwidth(560)
+		end
+	};
+};
