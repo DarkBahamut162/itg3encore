fe8b91567cd55cedad694e0d162c1ce56c47da89
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-06-10 16:20:28 +0200

    Add Pump Difficulties in Script/Colors.lua

--- Scripts/Colors.lua
+++ Scripts/Colors.lua
@@ -11,31 +11,49 @@ function DiffusePlayerColor( pn )
 end
 
 function CustomDifficultyToColor( dc )
+	--DDR
 	if dc == "Beginner"		then return color("#D05CF6") end
 	if dc == "Easy"			then return color("#09FF10") end
 	if dc == "Medium"		then return color("#F3F312") end
 	if dc == "Hard"			then return color("#EA3548") end
 	if dc == "Challenge"	then return color("#16AFF3") end
 	if dc == "Edit"			then return color("#F7F7F7") end
+	--PUMP
+	if dc == "Freestyle"	then return color("#09FF10") end
+	if dc == "HalfDouble"	then return color("#F3F312") end
+	if dc == "Crazy"		then return color("#EA3548") end
+	if dc == "Nightmare"	then return color("#16AFF3") end
 	return color("1,1,1,1")
 end
 
 function ContrastingDifficultyColor( dc )
+	--DDR
 	if dc == "Beginner"		then return color("#E2ABF5") end
 	if dc == "Easy"			then return color("#B2FFB5") end
 	if dc == "Medium"		then return color("#F2F2AA") end
 	if dc == "Hard"			then return color("#EBA4AB") end
 	if dc == "Challenge"	then return color("#AADCF2") end
 	if dc == "Edit"			then return color("#F7F7F7") end
+	--PUMP
+	if dc == "Freestyle"	then return color("#B2FFB5") end
+	if dc == "HalfDouble"	then return color("#F2F2AA") end
+	if dc == "Crazy"		then return color("#EBA4AB") end
+	if dc == "Nightmare"	then return color("#AADCF2") end
 	return color("1,1,1,1")
 end
 
 function DifficultyToColor( dc )
-	if dc == 'Difficulty_Beginner'	then return CustomDifficultyToColor("Beginner") end
-	if dc == 'Difficulty_Easy'		then return CustomDifficultyToColor("Easy") end
-	if dc == 'Difficulty_Medium'	then return CustomDifficultyToColor("Medium") end
-	if dc == 'Difficulty_Hard'		then return CustomDifficultyToColor("Hard") end
-	if dc == 'Difficulty_Challenge'	then return CustomDifficultyToColor("Challenge") end
-	if dc == 'Difficulty_Edit'		then return CustomDifficultyToColor("Edit") end
+	--DDR
+	if dc == 'Difficulty_Beginner'		then return CustomDifficultyToColor("Beginner") end
+	if dc == 'Difficulty_Easy'			then return CustomDifficultyToColor("Easy") end
+	if dc == 'Difficulty_Medium'		then return CustomDifficultyToColor("Medium") end
+	if dc == 'Difficulty_Hard'			then return CustomDifficultyToColor("Hard") end
+	if dc == 'Difficulty_Challenge'		then return CustomDifficultyToColor("Challenge") end
+	if dc == 'Difficulty_Edit'			then return CustomDifficultyToColor("Edit") end
+	--PUMP
+	if dc == 'Difficulty_Freestyle'		then return CustomDifficultyToColor("Easy") end
+	if dc == 'Difficulty_HalfDouble'	then return CustomDifficultyToColor("Medium") end
+	if dc == 'Difficulty_Crazy'			then return CustomDifficultyToColor("Hard") end
+	if dc == 'Difficulty_Nightmare'		then return CustomDifficultyToColor("Challenge") end
 	return color("1,1,1,1")
 end
