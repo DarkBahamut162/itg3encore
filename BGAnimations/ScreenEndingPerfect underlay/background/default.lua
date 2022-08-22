55ab1c658641f0ac4687633af9958b816e7a2df0
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-08-22 08:30:47 +0200

    Added ScreenEndingPerfect

---
+++ BGAnimations/ScreenEndingPerfect underlay/background/default.lua
@@ -0,0 +1,14 @@
+return Def.ActorFrame{
+    LoadActor("b1")..{
+		InitCommand=function(self) self:Center() end;
+		OnCommand=function(self) self:diffusealpha(0):sleep(5):zoomtowidth(SCREEN_WIDTH*2):zoomtoheight(SCREEN_HEIGHT*2):linear(10):diffusealpha(1):zoomtowidth(SCREEN_WIDTH*1.9):zoomtoheight(SCREEN_HEIGHT*1.9):linear(70):zoomtowidth(SCREEN_WIDTH):zoomtoheight(SCREEN_HEIGHT):linear(5):diffusealpha(0) end;
+    };
+    LoadActor("masktest")..{
+		InitCommand=function(self) self:Center():zoomtoheight(SCREEN_HEIGHT):zoomtowidth(SCREEN_WIDTH):blend("BlendMode_NoEffect"):zwrite(1) end;
+		OnCommand=function(self) self:diffusealpha(1):sleep(5):zoomtowidth(SCREEN_WIDTH*2):zoomtoheight(SCREEN_HEIGHT*2):linear(10):diffusealpha(1):zoomtowidth(SCREEN_WIDTH*1.9):zoomtoheight(SCREEN_HEIGHT*1.9):linear(70):zoomtowidth(SCREEN_WIDTH):zoomtoheight(SCREEN_HEIGHT):linear(5):diffusealpha(0) end;
+	};
+    LoadActor("865_jumpback")..{
+		InitCommand=function(self) self:blend(Blend.Add):Center():zoomtoheight(SCREEN_HEIGHT):zoomtowidth(SCREEN_WIDTH):ztest(1) end;
+		OnCommand=function(self) self:diffusealpha(0):sleep(10):zoomtowidth(SCREEN_WIDTH*2):zoomtoheight(SCREEN_HEIGHT*2):linear(5):diffusealpha(0.2):zoomtowidth(SCREEN_WIDTH*1.9):zoomtoheight(SCREEN_HEIGHT*1.9):linear(70):zoomtowidth(SCREEN_WIDTH):zoomtoheight(SCREEN_HEIGHT):linear(5):diffusealpha(0) end;
+	};
+};
--- BGAnimations/ScreenEndingPerfect underlay/default.lua
+++ BGAnimations/ScreenEndingPerfect underlay/default.lua
@@ -1,1 +1,10 @@
-return Def.ActorFrame{};
+return Def.ActorFrame{
+	Def.Quad{
+		InitCommand=function(self) self:FullScreen():diffuse(color("#000000FF")) end;
+	};
+    LoadActor("background");
+    LoadActor("people");
+    LoadActor("in the groove")..{
+        OnCommand=function(self) self:sleep(20) end;
+    };
+};
