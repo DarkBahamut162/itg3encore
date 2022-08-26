b209c83557a03bfc7bfbe937593d78fc8cd2fd53
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-08-26 07:49:59 +0200

    Finish Danger Animation for Double

--- BGAnimations/ScreenGameplay underlay/danger.lua
+++ BGAnimations/ScreenGameplay underlay/danger.lua
@@ -39,7 +39,9 @@ return Def.ActorFrame{
 				HideCommand=function(self) self:stopeffect():stoptweening():linear(0.5):diffusealpha(0) end;
 			};
 		};
-		--[[
+	};
+	Def.ActorFrame{
+		Name="DangerDouble";
 		Def.ActorFrame{
 			Name="Double";
 			BeginCommand=function(self)
@@ -48,20 +50,36 @@ return Def.ActorFrame{
 				local bDoubles = (styleType == 'StyleType_OnePlayerTwoSides' or styleType == 'StyleType_TwoPlayersSharedSides')
 				self:visible(bDoubles)
 			end;
+			HealthStateChangedMessageCommand=function(self, param)
+				if param.PlayerNumber == GAMESTATE:GetMasterPlayerNumber() then
+					if param.HealthState == "HealthState_Danger" then
+						self:RunCommandsOnChildren(function(self) self:playcommand("Show") end);
+					else
+						self:RunCommandsOnChildren(function(self) self:playcommand("Hide") end);
+					end
+				end
+			end;
 			LoadActor(THEME:GetPathB("_shared","danger/double"))..{
-				InitCommand=function(self) end;
+				InitCommand=function(self) self:faderight(0.1):fadeleft(0.1):fadetop(0.1):fadebottom(0.1):stretchto(SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM):diffusealpha(0) end;
+				ShowCommand=function(self) self:linear(0.3):diffusealpha(1):diffuseshift():effectcolor1(color("1,0,0,0.3")):effectcolor2(color("1,0,0,0.8")) end;
+				HideCommand=function(self) self:stopeffect():stoptweening():linear(0.5):diffusealpha(0) end;
 			};
 			Def.Quad{
-				InitCommand=function(self) end;
+				InitCommand=function(self) self:faderight(0.1):fadeleft(0.1):stretchto(SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM):diffusealpha(0) end;
+				ShowCommand=function(self) self:linear(0.3):diffusealpha(1):diffuseshift():effectcolor1(color("1,0,0,0.3")):effectcolor2(color("1,0,0,0.8")) end;
+				HideCommand=function(self) self:stopeffect():stoptweening():linear(0.5):diffusealpha(0) end;
 			};
 			LoadActor(THEME:GetPathB("_shared","danger/_danger text"))..{
-				InitCommand=function(self) end;
+				InitCommand=function(self) self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y+140):diffusealpha(0) end;
+				ShowCommand=function(self) self:linear(0.3):diffusealpha(1) end;
+				HideCommand=function(self) self:stopeffect():stoptweening():linear(0.5):diffusealpha(0) end;
 			};
 			LoadActor(THEME:GetPathB("_shared","danger/_danger glow"))..{
-				InitCommand=function(self) end;
+				InitCommand=function(self) self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y+140):cropleft(-0.3):cropright(1):faderight(0.1):fadeleft(0.1) end;
+				ShowCommand=function(self) self:sleep(0.5):linear(0.7):cropleft(1):cropright(-0.3):sleep(0.8):queuecommand("Show") end;
+				HideCommand=function(self) self:stopeffect():stoptweening():linear(0.5):diffusealpha(0) end;
 			};
 		};
-		--]]
 	};
 	Def.ActorFrame{
 		Name="DangerP2";
