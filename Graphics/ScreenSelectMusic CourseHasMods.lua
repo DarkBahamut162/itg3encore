e2d4c5d411aa67ca77324fbefe8c176d89ded7de
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-09-02 08:11:46 +0200

    Update & showoff more manually cached stuff

--- Graphics/ScreenSelectMusic CourseHasMods.lua
+++ Graphics/ScreenSelectMusic CourseHasMods.lua
@@ -1,18 +1,15 @@
 return Def.ActorFrame{
 	LoadFont("_r bold stroke")..{
-		InitCommand=function(self) self:halign(0):diffusealpha(0):zoom(.8):sleep(1):linear(.1):zoom(.5):diffusealpha(1):shadowlength(0):diffuseramp():effectperiod(1):effectoffset(0):effectclock("beat"):effectcolor1(color("#693A3A")):effectcolor2(color("#FF0000")) end;
-		OffCommand=function(self) self:linear(.1):diffusealpha(0) end;
-		ShowCommand=function(self) self:visible(1) end;
-		HideCommand=function(self) self:visible(0) end;
-		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end;
-		CurrentCourseChangedMessageCommand=function(self) self:playcommand("Set") end;
-		CurrentTrailP1ChangedMessageCommand=function(self) self:playcommand("Set") end;
-		CurrentTrailP2ChangedMessageCommand=function(self) self:playcommand("Set") end;
+		InitCommand=function(self) self:halign(0) end,
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+		CurrentCourseChangedMessageCommand=function(self) self:playcommand("Set") end,
+		CurrentTrailP1ChangedMessageCommand=function(self) self:playcommand("Set") end,
+		CurrentTrailP2ChangedMessageCommand=function(self) self:playcommand("Set") end,
 		SetCommand=function(self)
-			local curSelection = nil;
-			local text = "";
+			local curSelection = nil
+			local text = ""
 			if GAMESTATE:IsCourseMode() then
-				curSelection = GAMESTATE:GetCurrentCourse();
+				curSelection = GAMESTATE:GetCurrentCourse()
 				if curSelection then
 					if curSelection:HasMods() or curSelection:HasTimedMods() then
 						text = "HAS MODS"
@@ -22,61 +19,141 @@ return Def.ActorFrame{
 						local entries = trail:GetTrailEntries()
 						for i=1,#entries do
 							if entries[i]:GetNormalModifiers() ~= "" then
-								if text == "" then
-									text = "HAS SONG MODS"
-								else
-									text = text.."\nHAS SONG MODS"
-								end
+								if text ~= "" then text = text.."\n" end
+								text = text.."HAS SONG MODS"
 								break
 							end
 						end
 					end
-				end;
+				end
 			else
-				curSelection = GAMESTATE:GetCurrentSong();
-				curStep = GAMESTATE:GetCurrentSteps(GAMESTATE:GetMasterPlayerNumber());
+				curSelection = GAMESTATE:GetCurrentSong()
+				curStep = GAMESTATE:GetCurrentSteps(GAMESTATE:GetMasterPlayerNumber())
 				if curSelection then
-					if HasLuaCheck() then
-						text = "HAS LUA"
-					end;
 					if curStep then
-						if false then -- load/save cache StepCounter
-							local output = getStats(curStep);
-							text = ""
-							for i=1,#output do
-								text = text .. output[i]
-								if i ~= #output then text = text .. "|" end
+						if false then -- check for BGChanges since OutFox hasn't fixed the ingame offset sync overwrite yet
+							local BGCs = GetSMParameter(curSelection,"BGCHANGES")
+							if #BGCs > 0 then
+								text = "HAS BGCHANGES"
 							end
 						end
-						if false then -- display if Step has any timingData
-							local timingData = curStep:GetTimingData();
-							local bpms = timingData:GetBPMsAndTimes(true);
-							local stops = timingData:GetStops(true);
-							local delays = timingData:GetDelays(true);
-							local warps = timingData:GetWarps(true);
-							local fakes = timingData:GetFakes(true);
-							local scrolls = timingData:GetScrolls(true);
-							local speeds = timingData:GetSpeeds(true);
+						if false then -- load cache HasLua
+							if not LoadModule("Config.Exists.lua")("HasLua",getStepCacheFile(curStep)) then cacheStep(curStep) end
+							if FILEMAN:DoesFileExist(getStepCacheFile(curStep)) then
+								if tobool(LoadModule("Config.Load.lua")("HasLua",getStepCacheFile(curStep))) then
+									if text ~= "" then text = text .. "\n" end text = text .. "HAS LUA"
+								end
+							end
+						end
+						if true then -- load cache StepCounter
+							if text ~= "" then text = text .. "\n" end text = text .. table.concat(getStepCounter(curStep),"|")
+						end
+						if false then -- display data if Step has any timingData
+							if curSelection:IsStepsUsingDifferentTiming(curStep) then
+								if text ~= "" then text = text.."\n" end text = text.."SPLIT TIMING"
+							end
+							local timingData = curStep:GetTimingData()
+							local bpms = #timingData:GetBPMsAndTimes(true)
+							local stops = #timingData:GetStops(true)
+							local delays = #timingData:GetDelays(true)
+							local warps = #timingData:GetWarps(true)
+							local fakes = #timingData:GetFakes(true)
+							local scrolls = #timingData:GetScrolls(true)
+							local speeds = #timingData:GetSpeeds(true)
 
-							if #bpms > 1 then if text == "" then text = "HAS BPM CHANGES" else text = text.."\nHAS BPM CHANGES" end end
-							if #stops > 1 then if text == "" then text = "HAS STOPS" else text = text.."\nHAS STOPS" end end
-							if #delays > 0 then if text == "" then text = "HAS DELAYS" else text = text.."\nHAS DELAYS" end end
-							if #warps > 0 then if text == "" then text = "HAS WARPS" else text = text.."\nHAS WARPS" end end
-							if #fakes > 0 then if text == "" then text = "HAS FAKES" else text = text.."\nHAS FAKES" end end
-							if #scrolls > 1 then if text == "" then text = "HAS SCROLL CHANGES" else text = text.."\nHAS SCROLL CHANGES" end end
-							if #speeds > 1 then if text == "" then text = "HAS SPEED CHANGES" else text = text.."\nHAS SPEED CHANGES" end end
+							if bpms > 1 then		if text ~= "" then	text = text.."\n" end text = text.."HAS BPM CHANGES".." ("..(bpms-1)..")" end
+							if stops > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS STOPS".." ("..(stops)..")" end
+							if delays > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS DELAYS".." ("..(delays)..")" end
+							if warps > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS WARPS".." ("..(warps)..")" end
+							if fakes > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS FAKES".." ("..(fakes)..")" end
+							if scrolls > 1 then		if text ~= "" then	text = text.."\n" end text = text.."HAS SCROLL CHANGES".." ("..(scrolls)..")" end
+							if speeds > 1 then		if text ~= "" then	text = text.."\n" end text = text.."HAS SPEED CHANGES".." ("..(speeds)..")" end
+						end
+						if false then -- Get true BPM range
+							local timingdata = curStep:GetTimingData()
+							local bpms = curStep:GetDisplayBpms()
+							local truebpms = timingdata:GetActualBPM()
+
+							bpms[1]=math.round( bpms[1] * 1000 ) / 1000
+							bpms[2]=math.round( bpms[2] * 1000 ) / 1000
+							truebpms[1]=math.round( truebpms[1] * 1000 ) / 1000
+							truebpms[2]=math.round( truebpms[2] * 1000 ) / 1000
+
+							if bpms[1] == truebpms[1] and bpms[2] == truebpms[2] and bpms[1] == bpms[2] then
+								if text ~= "" then text = text.."\n" end text = text .. "BPM: "..truebpms[1]
+							else
+								local sets = timingdata:GetBPMsAndTimes()
+								local currentSet, lastSet
+								local duration, lastDuration, fastestBPM = 0, 0, 0
+
+								for i, set in ipairs(sets) do
+									currentSet = split("=",set)
+									currentSet[2]=math.round( currentSet[2] * 1000 ) / 1000
+
+									if lastSet then
+										duration = (currentSet[1]-lastSet[1]) / lastSet[2] * 60
+										if math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) >= 0.98 and math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) <= 1.02 then
+											duration = duration + lastDuration
+											if truebpms[1] <= tonumber(currentSet[2]) and truebpms[2] >= tonumber(currentSet[2]) then
+												if fastestBPM < currentSet[2] then fastestBPM = currentSet[2] end
+											end
+										end
+										if duration > 3 then
+											if truebpms[1] <= tonumber(lastSet[2]) and truebpms[2] >= tonumber(lastSet[2]) then
+												if fastestBPM < lastSet[2] then fastestBPM = lastSet[2] end
+											end
+											if truebpms[1] <= tonumber(currentSet[2]) and truebpms[2] >= tonumber(currentSet[2]) then
+												if math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) >= 0.98 and math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) <= 1.02 then
+													if fastestBPM < currentSet[2] then fastestBPM = currentSet[2] end
+												end
+											end
+										end
+									end
+									lastSet = currentSet
+									lastDuration = duration
+								end
+
+								duration = (curSelection:GetLastBeat()-lastSet[1]) / lastSet[2] * 60
+								if math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) >= 0.98 and math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) <= 1.02 then
+									duration = duration + lastDuration
+								end
+								if duration > 3 then
+									if truebpms[1] <= tonumber(lastSet[2]) and truebpms[2] >= tonumber(lastSet[2]) then
+										if fastestBPM < lastSet[2] then fastestBPM = lastSet[2] end
+									end
+								end
+
+								if fastestBPM == truebpms[1] then
+									if text ~= "" then text = text.."\n" end text = text .. "BPM: "..truebpms[1] .. " (" .. truebpms[2] .. ")"
+								elseif fastestBPM < truebpms[2] then
+									if text ~= "" then text = text.."\n" end text = text .. "BPM: "..truebpms[1] .. " - " .. fastestBPM .. " (" .. truebpms[2] .. ")"
+								else
+									if text ~= "" then text = text.."\n" end text = text .. "BPM: "..truebpms[1] .. " - " .. truebpms[2]
+								end
+							end
+						end
+						if false then -- check on OutFox's LastSecondHint fix for wrong cache data
+							local totalsecond = GAMESTATE:GetCurrentSong():MusicLengthSeconds()
+							local firstsecond = GAMESTATE:GetCurrentSong():GetFirstSecond()
+							local lastsecond = GAMESTATE:GetCurrentSong():GetLastSecond()
+							local difference = totalsecond - lastsecond
+							local trueFirstSecond = 0
+							local trueLastSecond = 0
+							if not LoadModule("Config.Exists.lua")("TrueFirstSecond",getStepCacheFile(curStep)) or not LoadModule("Config.Exists.lua")("TrueLastSecond",getStepCacheFile(curStep)) then cacheStep(curStep) end
+							if FILEMAN:DoesFileExist(getStepCacheFile(curStep)) then
+								trueFirstSecond = LoadModule("Config.Load.lua")("TrueFirstSecond",getStepCacheFile(curStep))
+								trueLastSecond = LoadModule("Config.Load.lua")("TrueLastSecond",getStepCacheFile(curStep))
+							end
+							if text ~= "" then text = text.."\n" end
+							text = text ..
+								"FIRST/TRUE sec: "..string.format("%0.3f",firstsecond).."/"..string.format("%0.3f",trueFirstSecond)..
+								"\nLAST/TRUE sec: "..string.format("%0.3f",lastsecond).."/"..string.format("%0.3f",trueLastSecond)..
+								"\nDIFF/TRUE sec: "..string.format("%0.3f",difference).."/"..string.format("%0.3f",totalsecond-trueLastSecond)
 						end
 					end
-					if false and NeedsBPMfixCheck() then -- check for OutFox's "Extreme BPM to Warp" for possible desync
-						if text == "" then text = "POTENTIAL WRONG WARPS!" else text = text.."\nPOTENTIAL WRONG WARPS!" end
-					end
-					if false then -- check on OutFox's LastSecondHint fix for wrong cache data
-						local content = NeedsSecsFixCheck()
-						text = "TOTAL sec: "..content[1].."\nLAST sec: "..content[2].."\nDIFF sec: "..content[3]
-					end
-				end;
-			end;
-			self:settext( text ):valign(1);
-		end;
-	};
-};
+				end
+			end
+			self:settext( text ):valign(1)
+		end
+	}
+}
--- Scripts/SelMusic.lua
+++ Scripts/SelMusic.lua
@@ -20,7 +20,7 @@ function TextBannerAfterSet(self,param)
 	local Title=self:GetChild("Title")
 	local Subtitle=self:GetChild("Subtitle")
 
-	if Subtitle:GetText() == "" then 
+	if Subtitle:GetText() == "" then
 		Title:y(0)
 		Subtitle:visible(false)
 	else
@@ -34,7 +34,7 @@ function CourseTextBannerAfterSet(self,param)
 	local Title=self:GetChild("Title")
 	local Subtitle=self:GetChild("Subtitle")
 
-	if Subtitle:GetText() == "" then 
+	if Subtitle:GetText() == "" then
 		Title:y(0)
 		Title:zoom(0.773)
 		Subtitle:visible(false)
@@ -51,7 +51,6 @@ function GetScreenSelectMusicHelpText()
 	local ret = THEME:GetString("ScreenSelectMusic", "HelpTextNormal")
 
 	if  SelectButtonAvailable() then
-		-- Show the help text if it's available.  This should match ScreenSelectMusic::SelectMenuAvailable.
 		if DifficultyChangingAvailable() or ModeMenuAvailable() then
 			ret = ret .. "::" .. THEME:GetString( "ScreenSelectMusic", "SelectButtonAvailableHelpTextAppend" )
 		end
@@ -72,88 +71,70 @@ function GetScreenSelectMusicHelpText()
 	return ret
 end
 
---[ja] SMファイルで指定したパラメータの内容を読み取る
---[en] Read the contents of the parameters specified in the SM file.
-function GetSMParameter(song,prm)
-	local st=song:GetAllSteps()
-	if #st<1 then
-		return ""
-	end
-	local t
-	t=st[1]:GetFilename()
-	if not FILEMAN:DoesFileExist(t) then
-		return ""
-	end
-	--[ja] 形式ではじく
-	--[en] burst out in a formal manner
-	local lt=string.lower(t)
-	if not string.find(lt,".*%.sm") and not string.find(lt,".*%.ssc") then
-		return ""
-	end
-	local f=RageFileUtil.CreateRageFile()
-	f:Open(t,1)
-	--[ja] 複数行を考慮していったん別変数に代入する
-	--[en] Assign to a separate variable once to account for multiple lines
-	local gl=""
-	local pl=string.lower(prm)
-	local l
+function GetSMParameter(song,parameter)
+	local steps = song:GetAllSteps()
+	if #steps<1 then return "" end
+	local fileName = steps[1]:GetFilename()
+	if not FILEMAN:DoesFileExist(fileName) then return "" end
+	local lowercaseFileName = string.lower(fileName)
+	if not string.find(lowercaseFileName,".*%.sm") and not string.find(lowercaseFileName,".*%.ssc") then return "" end
+	local file = RageFileUtil.CreateRageFile()
+	file:Open(fileName,1)
+	file:Seek(0)
+	local gLine = ""
+	local lowercaseParameter = string.lower(parameter)
+	local line
 	while true do
-		if f then
-			l=f:GetLine()
-			local ll=string.lower(l)
-			if string.find(ll,"#notes:.*") or f:AtEOF() then
-				break
-			--[ja] BOM考慮して .* を頭につける
-			--[en] BOM considerations . * at the beginning
-			elseif (string.find(ll,"^.*#"..pl..":.*") and (not string.find(ll,"^%/%/.*"))) or gl~="" then
-				gl=gl..""..split("//",l)[1]
-				if string.find(ll,".*;") then
-					break
-				end
+		if file then
+			line = file:GetLine()
+			local lowercaseLine = string.lower(line)
+			if string.find(lowercaseLine,"#notes:.*") or file:AtEOF() then break
+			elseif (string.find(lowercaseLine,"^.*#"..lowercaseParameter..":.*") and (not string.find(lowercaseLine,"^%/%/.*"))) or gLine ~= "" then
+				gLine = gLine..""..split("//",line)[1]
+				if string.find(lowercaseLine,".*;") then break end
 			end
 		end
 	end
-	local tmp={}
-	if gl=="" then
-		tmp={""}
+	local tmp = {}
+	if gLine == "" then
+		tmp = {""}
 	else
-		tmp=split(":",gl)
-		if tmp[2]==";" then
-			tmp[1]=""
+		tmp = split(":",gLine)
+		if tmp[2] == ";" then
+			tmp[1] = ""
 		else
-			if #tmp>2 then
-				tmp[1]=tmp[2]
-				for i=3,#tmp do
-					tmp[1]=tmp[1]..":"..split(";",tmp[i])[1]
+			if #tmp > 2 then
+				tmp[1] = tmp[2]
+				for i = 3, #tmp do
+					tmp[1] = tmp[1]..":"..split(";",tmp[i])[1]
 				end
 			else
-				tmp[1]=split(";",tmp[2])[1]
+				tmp[1] = split(";",tmp[2])[1]
 			end
 		end
 	end
-	f:Close()
-	f:destroy()
+	file:Close()
+	file:destroy()
 	return tmp[1]
 end
 
 function HasLua(song,changes)
 	local var = GetSMParameter(song,changes)
-	local prm, cur
-	local output = false
+	local parameter, current
 	if var ~= "" then
-		prm = split(",",var)
-		if prm ~= "" then
-			for i=1,#prm do
-				prm[i] = split("=",prm[i])
-				if #prm[i] >= 2 then
-					cur = prm[i][2]
-					if cur ~= "" then
-						if string.find(cur,".lua",0,true) then 
-							output = true
-						elseif string.find(cur,".",0,true) then else
-							if string.find(cur,"-nosongbg-",0,true) or string.find(cur,"-random-",0,true)
-								or string.find(cur,"songbackground",0,true) or cur == "" then else
-								output = true
+		parameter = split(",",var)
+		if parameter ~= "" then
+			for i=1,#parameter do
+				parameter[i] = split("=",parameter[i])
+				if #parameter[i] >= 2 then
+					current = parameter[i][2]
+					if current ~= "" then
+						if string.find(current,".lua",0,true) then
+							return true
+						elseif string.find(current,".",0,true) then else
+							if string.find(current,"-nosongbg-",0,true) or string.find(current,"-random-",0,true)
+								or string.find(current,"songbackground",0,true) or current == "" then else
+								return true
 							end
 						end
 					end
@@ -161,176 +142,141 @@ function HasLua(song,changes)
 			end
 		end
 	end
-	return output
-end
-
-function HasImage(song,changes)
-	local var = GetSMParameter(song,changes)
-	local prm, cur
-	local output = false
-	if var ~= "" then
-		prm = split(",",var)
-		if prm ~= "" then
-			for i=1,#prm do
-				prm[i] = split("=",prm[i])
-				if #prm[i] >= 2 then
-					cur = prm[i][2]
-					if cur ~= "" then
-						if string.find(cur,".jpg",0,true) or string.find(cur,".jpeg",0,true) or
-							string.find(cur,".gif",0,true) or string.find(cur,".png",0,true) or
-							string.find(cur,".bmp",0,true) then
-							output = true
-						end
-					end
-				end
-			end
-		end
-	end
-	return output
-end
-
-function HasVideo(song,changes)
-	local var = GetSMParameter(song,changes)
-	local prm, cur
-	local output = false
-	if var ~= "" then
-		prm = split(",",var)
-		if prm ~= "" then
-			for i=1,#prm do
-				prm[i] = split("=",prm[i])
-				if #prm[i] >= 2 then
-					cur = prm[i][2]
-					if cur ~= "" then
-						if string.find(cur,".ogv",0,true) or string.find(cur,".avi",0,true) or
-							string.find(cur,".mov",0,true) or string.find(cur,".mpg",0,true) or
-							string.find(cur,".mpeg",0,true) or string.find(cur,".wmv",0,true) or
-							string.find(cur,".flv",0,true) or string.find(cur,".mp4",0,true) then
-							output = true
-						end
-					end
-				end
-			end
-		end
-	end
-	return output
-end
-
-function NeedsBPMfix(song)
-	local var = GetSMParameter(song,"BPMS")
-	local prm, cur
-	local output = false
-	if var ~= "" then
-		prm = split(",",var)
-		if prm ~= "" then
-			for i=1,#prm do
-				prm[i] = split("=",prm[i])
-				if #prm[i] >= 2 then
-					cur = prm[i][2]
-					if cur ~= "" then
-						if tonumber(cur) >= 109713 then
-							output = true
-						end
-					end
-				end
-			end
-		end
-	end
-	return output
-end
-
---Extreme BPMs to warp desync check
-function NeedsBPMfixCheck()
-	local song = GAMESTATE:GetCurrentSong()
-	return NeedsBPMfix(song)
-end
-
---False LASTSECONDHINT fix check
-function NeedsSecsFixCheck()
-	local song = GAMESTATE:GetCurrentSong()
-	local totalsecond = song:MusicLengthSeconds()
-	local lastsecond = song:GetLastSecond()
-	local difference = totalsecond - lastsecond
-	return {totalsecond, lastsecond, difference}
+	return false
 end
 
 function HasLuaCheck()
-	local song = GAMESTATE:GetCurrentSong();
+	local song = GAMESTATE:GetCurrentSong()
 	return HasLua(song,"bgchanges") or HasLua(song,"fgchanges")
 end
 
-function HasVideoCheck()
-	local song = GAMESTATE:GetCurrentSong();
-	return HasVideo(song,"bgchanges") or HasVideo(song,"fgchanges")
-end
-
-function HasImageCheck()
-	local song = GAMESTATE:GetCurrentSong();
-	return HasImage(song,"bgchanges") or HasImage(song,"fgchanges")
-end
-
 function IsCourseSecret()
-	local isSecret = false
-
 	if GAMESTATE:IsCourseMode() then
 		for i=1,#GAMESTATE:GetCurrentCourse():GetCourseEntries() do
 			if GAMESTATE:GetCurrentCourse():GetCourseEntry(i-1):IsSecret() then
-				isSecret = true
+				return true
 			end
 		end
 	end
 
-	return isSecret
+	return false
 end
 
 local allowednotes = {
 	["TapNoteType_Tap"] = true,
 	["TapNoteType_Lift"] = true,
-	-- Support the heads of the subtypes.
 	["TapNoteSubType_Hold"] = true,
-	["TapNoteSubType_Roll"] = true,
+	["TapNoteSubType_Roll"] = true
 }
 
-function getStats(Steps)
-    local chartint = 1
-	local currentBeat = 0;
-	local currentNotes = 0;
-	local noteCounter = {0,0,0,0};
-	local lastRegisteredBeat = 0;
-	local hash = 0;
+function getStepCacheFile(Steps)
+	local hashOld = false
+	local cache = ""
+	if hashOld then
+		cache = "Cache/Steps/"..Steps:GetHash()
+	else
+		local filename = split("/",curStep:GetFilename())
+		cache = "Cache/Steps/Steps_"..filename[3].."_"..filename[4].."_"..ToEnumShortString(Steps:GetStepsType()).."_"..ToEnumShortString(Steps:GetDifficulty())
+	end
+	return cache
+end
 
-    if Steps then
-        for k,v in pairs( GAMESTATE:GetCurrentSong():GetAllSteps() ) do
-            if v == Steps then
-				chartint = k
-				hash = Steps:GetHash()
-				break
+function cacheStep(Steps)
+    local chartint = 1
+	local currentBeat = 0
+	local currentNotes = 0
+	local currentMines = 0
+	local noteCounter
+	local firstBeat = 999
+	local lastBeat = 0
+	local shockArrows = ""
+	local arrows = GAMESTATE:GetCurrentStyle():ColumnsPerPlayer()
+	local temp = ""
+	local lastSecondHint = GetSMParameter(GAMESTATE:GetCurrentSong(),"LASTSECONDHINT")
+
+	for k,v in pairs( GAMESTATE:GetCurrentSong():GetAllSteps() ) do
+		if v == Steps then
+			chartint = k
+			break
+		end
+	end
+
+	for i=1,arrows do
+		temp = temp .. "0"
+		if i < arrows then temp = temp .. "," end
+	end
+
+	noteCounter = split(",",temp)
+
+	if lastSecondHint ~= "" then
+		lastBeat = Steps:GetTimingData():GetBeatFromElapsedTime(lastSecondHint)
+	end
+
+	local timingData = Steps:GetTimingData()
+	local warps = timingData:GetWarps()
+	local ignore = false
+
+	if #warps > 0 then
+		for i=1, #warps do
+			warps[i] = split("=",warps[i])
+			warps[i][1] = tonumber(warps[i][1])
+			warps[i][2] = tonumber(warps[i][2])
+		end
+	end
+
+	for k,v in pairs( GAMESTATE:GetCurrentSong():GetNoteData(chartint) ) do
+		ignore = false
+		if currentBeat < v[1] then
+			currentBeat = v[1]
+			if currentNotes ~= 0 then
+				noteCounter[currentNotes] = noteCounter[currentNotes] + 1
 			end
-        end
-		if hash > 0 then
-			if LoadModule("Config.Exists.lua")(hash.."_","Cache/Steps/"..hash) then
-				return split("_",LoadModule("Config.Load.lua")(hash.."_","Cache/Steps/"..hash))
-			else
-				for k,v in pairs( GAMESTATE:GetCurrentSong():GetNoteData(chartint) ) do
-					if currentBeat < v[1] then
-						lastRegisteredBeat = currentBeat
-						currentBeat = v[1]
-						if currentNotes ~= 0 then 
-							noteCounter[currentNotes] = noteCounter[currentNotes] + 1
-						end
-						currentNotes = 0
-					end
-					if allowednotes[ v[3] ] then
-						currentNotes = currentNotes + 1
-					end
+			currentNotes, currentMines = 0, 0
+		end
+
+		if #warps > 0 then
+			for i=1, #warps do
+				if warps[i][1] < currentBeat and (warps[i][1] + warps[i][2]) > currentBeat then
+					ignore = true
+					break
 				end
-				if lastRegisteredBeat < currentBeat then
-					if currentNotes ~= 0 then
-						noteCounter[currentNotes] = noteCounter[currentNotes] + 1
-					end
-				end
-				LoadModule("Config.Save.lua")(hash.."_",table.concat(noteCounter, "_"),"Cache/Steps/"..hash)
 			end
 		end
-    end
-    return {noteCounter[1],noteCounter[2],noteCounter[3],noteCounter[4]}
+
+		if not ignore then
+			if allowednotes[ v[3] ] then
+				if v["length"] then
+					if currentBeat + v["length"] > lastBeat and lastSecondHint == "" then lastBeat = currentBeat + v["length"] end
+				else
+					if currentBeat > lastBeat and lastSecondHint == "" then lastBeat = currentBeat end
+				end
+				if currentBeat < firstBeat then firstBeat = currentBeat end
+				currentNotes = currentNotes + 1
+			elseif v[3] == "TapNoteType_Mine" then
+				currentMines = currentMines + 1
+				if currentMines == arrows then
+					if shockArrows ~= "" then
+						shockArrows = shockArrows .. "_"
+					end
+					shockArrows = shockArrows .. "_" .. Steps:GetTimingData():GetElapsedTimeFromBeat(v[1])
+				end
+			end
+		end
+	end
+	if currentNotes ~= 0 then
+		noteCounter[currentNotes] = noteCounter[currentNotes] + 1
+	end
+
+	LoadModule("Config.Save.lua")("StepCounter",table.concat(noteCounter,"_"),getStepCacheFile(Steps))
+	LoadModule("Config.Save.lua")("TrueFirstBeat",firstBeat,getStepCacheFile(Steps))
+	LoadModule("Config.Save.lua")("TrueFirstSecond",Steps:GetTimingData():GetElapsedTimeFromBeat(firstBeat),getStepCacheFile(Steps))
+	LoadModule("Config.Save.lua")("TrueLastBeat",lastBeat,getStepCacheFile(Steps))
+	LoadModule("Config.Save.lua")("TrueLastSecond",Steps:GetTimingData():GetElapsedTimeFromBeat(lastBeat),getStepCacheFile(Steps))
+	LoadModule("Config.Save.lua")("HasLua",HasLuaCheck() and "true" or "false",getStepCacheFile(Steps))
+	if shockArrows ~= "" then LoadModule("Config.Save.lua")("ShockArrows",shockArrows,getStepCacheFile(Steps)) end
+end
+
+function getStepCounter(Steps)
+	if not LoadModule("Config.Exists.lua")("StepCounter",getStepCacheFile(Steps)) then cacheStep(Steps) end
+	return split("_",LoadModule("Config.Load.lua")("StepCounter",getStepCacheFile(Steps)))
 end
