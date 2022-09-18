5e221e120666edac9a2cf7ff9ac2171faf3fdadb
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-09-18 09:06:59 +0200

    Fix ScreenSelectStyle in (all because of CoinMode)

---
+++ BGAnimations/ScreenSelectStyle in/default.lua
@@ -0,0 +1,9 @@
+if GAMESTATE:GetCoinMode()=='CoinMode_Free' then
+    return Def.ActorFrame{
+        LoadActor(THEME:GetPathB("","_menu in"))
+    }
+elseif GAMESTATE:GetCoinMode()=='CoinMode_Home' then
+    return Def.ActorFrame{
+        LoadActor(THEME:GetPathB("","_fade in normal"))
+    }
+end
