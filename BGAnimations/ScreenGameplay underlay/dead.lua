195c98b49cff717ba8558893b01002bf0358ee16
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-09-24 18:53:13 +0200

    Fix dead background fade on PLAYER_2's side

--- BGAnimations/ScreenGameplay underlay/dead.lua
+++ BGAnimations/ScreenGameplay underlay/dead.lua
@@ -19,7 +19,7 @@ return Def.ActorFrame{
 		},
 		Def.Quad{
 			Name="DeadP2",
-			InitCommand=function(self) self:diffuse(color("0,0,0,0.5")):faderight(0.3):stretchto(SCREEN_CENTER_X,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM):diffusealpha(0):player(PLAYER_2) end,
+			InitCommand=function(self) self:diffuse(color("0,0,0,0.5")):fadeleft(0.3):stretchto(SCREEN_CENTER_X,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM):diffusealpha(0):player(PLAYER_2) end,
 			ShowCommand=function(self) self:linear(0.2):diffusealpha(0.5) end
 		}
 	},
