76b964a1f687197f6e7edb68da0b258d2b3b6ac4
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-10-05 09:11:32 +0200

    Add maxwidth to HelpDisplay

--- Graphics/ScreenWithMenuElements Help.lua
+++ Graphics/ScreenWithMenuElements Help.lua
@@ -4,6 +4,7 @@ return Def.HelpDisplay {
 		local s = THEME:GetString(Var "LoadingScreen","HelpText")
 		self:SetSecsBetweenSwitches(THEME:GetMetric("HelpDisplay","TipSwitchTime"))
 		self:SetTipsColonSeparated(s)
+		self:maxwidth(269)
 	end,
 	SetHelpTextCommand=function(self, params)
 		self:SetTipsColonSeparated( params.Text )
