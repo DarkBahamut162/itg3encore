ebe4eb97257c25d34475d338c3e298d3c81ba9e0
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-10-21 09:02:52 +0200

    Change StepCache, move timingData & add CalcDiff

--- Graphics/ScreenSelectMusic CourseHasMods.lua
+++ Graphics/ScreenSelectMusic CourseHasMods.lua
@@ -32,41 +32,17 @@ return Def.ActorFrame{
 				if curSelection then
 					if curStep then
 						if false then -- load cache HasLua OR check for BGChanges since OutFox hasn't fixed the ingame offset sync overwrite yet
-							if not LoadModule("Config.Exists.lua")("HasLua",getStepCacheFile(curStep)) then cacheStep(curStep) end
-							if FILEMAN:DoesFileExist(getStepCacheFile(curStep)) then
-								if tobool(LoadModule("Config.Load.lua")("HasLua",getStepCacheFile(curStep))) then
-									text = "HAS LUA"
-								else
-									local BGCs = GetSMParameter(curSelection,"BGCHANGES")
-									if #BGCs > 0 then
-										text = "HAS BGCHANGES"
-									end
+							if tobool(LoadFromCache(curStep,"HasLua")) then
+								text = "HAS LUA"
+							else
+								local BGCs = GetSMParameter(curSelection,"BGCHANGES")
+								if #BGCs > 0 then
+									text = "HAS BGCHANGES"
 								end
 							end
 						end
 						if false then -- load cache StepCounter
 							if text ~= "" then text = text .. "\n" end text = text .. table.concat(getStepCounter(curStep),"|")
-						end
-						if false then -- display data if Step has any timingData
-							if curSelection:IsStepsUsingDifferentTiming(curStep) then
-								if text ~= "" then text = text.."\n" end text = text.."SPLIT TIMING"
-							end
-							local timingData = curStep:GetTimingData()
-							local bpms = #timingData:GetBPMsAndTimes(true)
-							local stops = #timingData:GetStops(true)
-							local delays = #timingData:GetDelays(true)
-							local warps = #timingData:GetWarps(true)
-							local fakes = #timingData:GetFakes(true)
-							local scrolls = #timingData:GetScrolls(true)
-							local speeds = #timingData:GetSpeeds(true)
-
-							if bpms > 1 then		if text ~= "" then	text = text.."\n" end text = text.."HAS BPM CHANGES".." ("..(bpms-1)..")" end
-							if stops > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS STOPS".." ("..(stops)..")" end
-							if delays > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS DELAYS".." ("..(delays)..")" end
-							if warps > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS WARPS".." ("..(warps)..")" end
-							if fakes > 0 then		if text ~= "" then	text = text.."\n" end text = text.."HAS FAKES".." ("..(fakes)..")" end
-							if scrolls > 1 then		if text ~= "" then	text = text.."\n" end text = text.."HAS SCROLL CHANGES".." ("..(scrolls)..")" end
-							if speeds > 1 then		if text ~= "" then	text = text.."\n" end text = text.."HAS SPEED CHANGES".." ("..(speeds)..")" end
 						end
 						if false then -- Get true BPM range
 							local timingdata = curStep:GetTimingData()
@@ -91,18 +67,18 @@ return Def.ActorFrame{
 
 									if lastSet then
 										duration = (currentSet[1]-lastSet[1]) / lastSet[2] * 60
-										if math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) >= 0.98 and math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) <= 1.02 then
+										if math.abs(lastSet[2]/currentSet[2]) >= 0.98 and math.abs(lastSet[2]/currentSet[2]) <= 1.02 then
 											duration = duration + lastDuration
-											if truebpms[1] <= tonumber(currentSet[2]) and truebpms[2] >= tonumber(currentSet[2]) then
+											if truebpms[1] <= currentSet[2] and truebpms[2] >= currentSet[2] then
 												if fastestBPM < currentSet[2] then fastestBPM = currentSet[2] end
 											end
 										end
-										if duration >= 4 then
-											if truebpms[1] <= tonumber(lastSet[2]) and truebpms[2] >= tonumber(lastSet[2]) then
+										if duration >= 6 then
+											if truebpms[1] <= lastSet[2] and truebpms[2] >= lastSet[2] then
 												if fastestBPM < lastSet[2] then fastestBPM = lastSet[2] end
 											end
-											if truebpms[1] <= tonumber(currentSet[2]) and truebpms[2] >= tonumber(currentSet[2]) then
-												if math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) >= 0.98 and math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) <= 1.02 then
+											if truebpms[1] <= currentSet[2] and truebpms[2] >= currentSet[2] then
+												if math.abs(lastSet[2]/currentSet[2]) >= 0.98 and math.abs(lastSet[2]/currentSet[2]) <= 1.02 then
 													if fastestBPM < currentSet[2] then fastestBPM = currentSet[2] end
 												end
 											end
@@ -113,16 +89,16 @@ return Def.ActorFrame{
 								end
 
 								duration = (curSelection:GetLastBeat()-lastSet[1]) / lastSet[2] * 60
-								if math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) >= 0.98 and math.abs(tonumber(lastSet[2])/tonumber(currentSet[2])) <= 1.02 then
+								if math.abs(lastSet[2]/currentSet[2]) >= 0.98 and math.abs(lastSet[2]/currentSet[2]) <= 1.02 then
 									--duration = duration + lastDuration
 								end
 								if duration >= 4 then
-									if truebpms[1] <= tonumber(lastSet[2]) and truebpms[2] >= tonumber(lastSet[2]) then
+									if truebpms[1] <= lastSet[2] and truebpms[2] >= lastSet[2] then
 										if fastestBPM < lastSet[2] then fastestBPM = lastSet[2] end
 									end
 								end
 
-								if math.abs(tonumber(fastestBPM)/tonumber(truebpms[2])) >= 0.96 and math.abs(tonumber(fastestBPM)/tonumber(truebpms[2])) <= 1.04 then
+								if math.abs(fastestBPM/truebpms[2]) >= 0.96 and math.abs(fastestBPM/truebpms[2]) <= 1.04 then
 									fastestBPM = truebpms[2]
 								end
 
@@ -144,18 +120,40 @@ return Def.ActorFrame{
 							local firstsecond = GAMESTATE:GetCurrentSong():GetFirstSecond()
 							local lastsecond = GAMESTATE:GetCurrentSong():GetLastSecond()
 							local difference = totalsecond - lastsecond
-							local trueFirstSecond = 0
-							local trueLastSecond = 0
-							if not LoadModule("Config.Exists.lua")("TrueFirstSecond",getStepCacheFile(curStep)) or not LoadModule("Config.Exists.lua")("TrueLastSecond",getStepCacheFile(curStep)) then cacheStep(curStep) end
-							if FILEMAN:DoesFileExist(getStepCacheFile(curStep)) then
-								trueFirstSecond = LoadModule("Config.Load.lua")("TrueFirstSecond",getStepCacheFile(curStep))
-								trueLastSecond = LoadModule("Config.Load.lua")("TrueLastSecond",getStepCacheFile(curStep))
-							end
+							local trueFirstSecond = LoadFromCache(curStep,"TrueFirstSecond")
+							local trueLastSecond = LoadFromCache(curStep,"TrueLastSecond")
 							if text ~= "" then text = text.."\n" end
 							text = text ..
 								"FIRST/TRUE sec: "..string.format("%0.3f",firstsecond).."/"..string.format("%0.3f",trueFirstSecond)..
 								"\nLAST/TRUE sec: "..string.format("%0.3f",lastsecond).."/"..string.format("%0.3f",trueLastSecond)..
 								"\nDIFF/TRUE sec: "..string.format("%0.3f",difference).."/"..string.format("%0.3f",totalsecond-trueLastSecond)
+						end
+						if false then --Calculate Difficulty
+							local totalSeconds = LoadFromCache(curStep,"TrueLastSecond") - LoadFromCache(curStep,"TrueFirstSecond")
+							local stepCounter,stepSum = getStepCounter(curStep),0
+							local avg_bps_OLD= GAMESTATE:GetCurrentSong():GetLastBeat() / GAMESTATE:GetCurrentSong():MusicLengthSeconds()
+							local avg_bps_NEW= LoadModule("Config.Load.lua")("TrueLastBeat",getStepCacheFile(curStep)) / totalSeconds
+							local stream_OLD = curStep:GetRadarValues(GAMESTATE:GetMasterPlayerNumber()):GetValue('RadarCategory_Stream')
+							local stream_NEW = stream_OLD * GAMESTATE:GetCurrentSong():MusicLengthSeconds() / totalSeconds
+							local voltage_OLD = curStep:GetRadarValues(GAMESTATE:GetMasterPlayerNumber()):GetValue('RadarCategory_Voltage')
+							local voltage_NEW = voltage_OLD / avg_bps_OLD * avg_bps_NEW
+							local air_OLD = curStep:GetRadarValues(GAMESTATE:GetMasterPlayerNumber()):GetValue('RadarCategory_Air')
+							local air_NEW = air_OLD * GAMESTATE:GetCurrentSong():MusicLengthSeconds() / totalSeconds
+							local freeze_OLD = curStep:GetRadarValues(GAMESTATE:GetMasterPlayerNumber()):GetValue('RadarCategory_Freeze')
+							local freeze_NEW = freeze_OLD * GAMESTATE:GetCurrentSong():MusicLengthSeconds() / totalSeconds
+							local chaos_OLD = curStep:GetRadarValues(GAMESTATE:GetMasterPlayerNumber()):GetValue('RadarCategory_Chaos')
+							local chaos_NEW = chaos_OLD * GAMESTATE:GetCurrentSong():MusicLengthSeconds() / totalSeconds
+							for i=1,#stepCounter do if stepCounter[i] then stepSum = stepSum + stepCounter[i] end end
+							stepSum = math.round( ( stepSum / totalSeconds ) * (#stepCounter/2) )
+
+							if text ~= "" then text = text.."\n" end text = text .. "Calc'd Difficulty (DB162): "..stepSum
+							if text ~= "" then text = text.."\n" end text = text .. "Calc'd Difficulty (WAIEI): "..GetConvertDifficulty(curStep)
+							if text ~= "" then text = text.."\n" end text = text .. "Old GR: "..
+							string.format("%1.0f",stream_OLD*100).."|"..string.format("%1.0f",voltage_OLD*100).."|"..
+							string.format("%1.0f",air_OLD*100).."|"..string.format("%1.0f",freeze_OLD*100).."|"..string.format("%1.0f",chaos_OLD*100)
+							if text ~= "" then text = text.."\n" end text = text .. "New GR: "..
+							string.format("%1.0f",stream_NEW*100).."|"..string.format("%1.0f",voltage_NEW*100).."|"..
+							string.format("%1.0f",air_NEW*100).."|"..string.format("%1.0f",freeze_NEW*100).."|"..string.format("%1.0f",chaos_NEW*100)
 						end
 					end
 				end
--- Scripts/SelMusic.lua
+++ Scripts/SelMusic.lua
@@ -164,19 +164,19 @@ local allowednotes = {
 	["TapNoteSubType_Roll"] = true
 }
 
-function getStepCacheFile(Steps)
+function getStepCacheFile(Step)
 	local hashOld = false
 	local cache = ""
 	if hashOld then
-		cache = "Cache/Steps/"..Steps:GetHash()
+		cache = "Cache/Steps/"..Step:GetHash()
 	else
-		local filename = split("/",Steps:GetFilename())
-		cache = "Cache/Steps/Steps_"..filename[3].."_"..filename[4].."_"..ToEnumShortString(Steps:GetStepsType()).."_"..ToEnumShortString(Steps:GetDifficulty())
+		local filename = split("/",Step:GetFilename())
+		cache = "Cache/Steps/Steps_"..filename[3].."_"..filename[4].."_"..ToEnumShortString(Step:GetStepsType()).."_"..ToEnumShortString(Step:GetDifficulty())
 	end
 	return cache
 end
 
-function cacheStep(Steps)
+function cacheStep(Step)
     local chartint = 1
 	local currentBeat = 0
 	local currentNotes = 0
@@ -190,7 +190,7 @@ function cacheStep(Steps)
 	local lastSecondHint = GetSMParameter(GAMESTATE:GetCurrentSong(),"LASTSECONDHINT")
 
 	for k,v in pairs( GAMESTATE:GetCurrentSong():GetAllSteps() ) do
-		if v == Steps then
+		if v == Step then
 			chartint = k
 			break
 		end
@@ -204,10 +204,10 @@ function cacheStep(Steps)
 	noteCounter = split(",",temp)
 
 	if lastSecondHint ~= "" then
-		lastBeat = Steps:GetTimingData():GetBeatFromElapsedTime(lastSecondHint)
+		lastBeat = Step:GetTimingData():GetBeatFromElapsedTime(lastSecondHint)
 	end
 
-	local timingData = Steps:GetTimingData()
+	local timingData = Step:GetTimingData()
 	local warps = timingData:GetWarps()
 	local ignore = false
 
@@ -253,7 +253,7 @@ function cacheStep(Steps)
 					if shockArrows ~= "" then
 						shockArrows = shockArrows .. "_"
 					end
-					shockArrows = shockArrows .. Steps:GetTimingData():GetElapsedTimeFromBeat(v[1])
+					shockArrows = shockArrows .. Step:GetTimingData():GetElapsedTimeFromBeat(v[1])
 				end
 			end
 		end
@@ -262,18 +262,22 @@ function cacheStep(Steps)
 		noteCounter[currentNotes] = noteCounter[currentNotes] + 1
 	end
 
-	LoadModule("Config.Save.lua")("StepCounter",table.concat(noteCounter,"_"),getStepCacheFile(Steps))
-	LoadModule("Config.Save.lua")("TrueFirstBeat",firstBeat,getStepCacheFile(Steps))
-	LoadModule("Config.Save.lua")("TrueFirstSecond",Steps:GetTimingData():GetElapsedTimeFromBeat(firstBeat),getStepCacheFile(Steps))
-	LoadModule("Config.Save.lua")("TrueLastBeat",lastBeat,getStepCacheFile(Steps))
-	LoadModule("Config.Save.lua")("TrueLastSecond",Steps:GetTimingData():GetElapsedTimeFromBeat(lastBeat),getStepCacheFile(Steps))
-	LoadModule("Config.Save.lua")("HasLua",HasLuaCheck() and "true" or "false",getStepCacheFile(Steps))
-	if shockArrows ~= "" then LoadModule("Config.Save.lua")("ShockArrows",shockArrows,getStepCacheFile(Steps)) end
+	LoadModule("Config.Save.lua")("StepCounter",table.concat(noteCounter,"_"),getStepCacheFile(Step))
+	LoadModule("Config.Save.lua")("TrueFirstBeat",firstBeat,getStepCacheFile(Step))
+	LoadModule("Config.Save.lua")("TrueFirstSecond",Step:GetTimingData():GetElapsedTimeFromBeat(firstBeat),getStepCacheFile(Step))
+	LoadModule("Config.Save.lua")("TrueLastBeat",lastBeat,getStepCacheFile(Step))
+	LoadModule("Config.Save.lua")("TrueLastSecond",Step:GetTimingData():GetElapsedTimeFromBeat(lastBeat),getStepCacheFile(Step))
+	LoadModule("Config.Save.lua")("HasLua",HasLuaCheck() and "true" or "false",getStepCacheFile(Step))
+	if shockArrows ~= "" then LoadModule("Config.Save.lua")("ShockArrows",shockArrows,getStepCacheFile(Step)) end
 end
 
-function getStepCounter(Steps)
-	if not LoadModule("Config.Exists.lua")("StepCounter",getStepCacheFile(Steps)) then cacheStep(Steps) end
-	return split("_",LoadModule("Config.Load.lua")("StepCounter",getStepCacheFile(Steps)))
+function LoadFromCache(Step,value)
+	if not LoadModule("Config.Exists.lua")(value,getStepCacheFile(Step)) then cacheStep(Step) end
+	return LoadModule("Config.Load.lua")(value,getStepCacheFile(Step))
+end
+
+function getStepCounter(Step)
+	return split("_",LoadFromCache(Step,"StepCounter"))
 end
 
 function GetMinSecondsToStep()
@@ -303,4 +307,85 @@ function GetMinSecondsToStep()
 	if firstBeat < 999 then firstSec = math.max(firstBeat * 60 / firstBpm, -1) end
 	firstSec = song:GetFirstSecond() - firstSec + offset + 0.05
 	return math.max(firstSec, 1.55)
+end
+
+local tapsp2lv100={
+	00.5,0.55,00.6,0.65,00.7,0.75,00.8,0.85,00.9,0.95,
+	01.0,01.2,01.4,01.6,01.7,01.8,01.9,02.0,02.2,02.3,
+	02.5,02.6,03.0,03.3,03.6,04.0,04.3,04.6,05.0,05.2,
+	05.5,05.7,06.0,06.2,06.5,06.7,07.0,07.2,07.4,07.6,
+	07.8,08.0,08.3,08.6,09.0,09.2,09.4,09.6,09.8,10.0,
+	10.2,10.5,10.7,11.0,11.3,11.6,12.0,12.0,12.2,12.4,
+	12.6,12.8,13.0,13.0,13.3,13.5,13.7,14.0,14.2,14.4,
+	14.5,14.5,14.6,14.8,15.0,15.2,15.4,15.6,15.8,16.0,
+	16.2,16.4,16.6,16.8,17.0,17.2,17.4,17.6,17.8,18.0,
+	18.2,18.4,18.6,18.8,19.0,19.2,19.4,19.6,19.8,20.0,
+	20.2,20.4,20.6,20.8,21.0,21.2,21.4,21.6,21.8,22.0,
+	22.2,22.4,22.6,22.8,23.0,23.2,23.4,23.6,23.8,24.0,
+	24.2,24.4,24.6,24.8,25.0,25.2,25.4,25.6,25.8,26.0,
+	26.2,26.4,26.6,26.8,27.0,27.2,27.4,27.6,27.8,28.0,
+	28.2,28.4,28.6,28.8,29.0,29.2,29.4,29.6,29.8,30.0,
+	30.2,30.4,30.6,30.8,31.0,31.2,31.4,31.6,31.8,32.0,
+	32.2,32.4,32.6,32.8,33.0,33.2,33.4,33.6,33.8,34.0,
+	34.2,34.4,34.6,34.8,35.0,35.2,35.4,35.6,35.8,36.0,
+	36.4,36.8,37.2,37.6,38.0,38.4,38.8,39.2,39.6,40.0,
+	40.5,41.0,41.5,42.0,42.5,43.0,43.5,44.0,44.5,45.0,
+	45.5,46.0,46.5,47.0,47.5,48.0,48.5,49.0,49.5,50.0
+}
+
+function GetConvertDifficulty(Step)
+	local songLength = LoadFromCache(Step,"TrueLastSecond") - LoadFromCache(Step,"TrueFirstSecond")
+	local voltage=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Voltage')*GAMESTATE:GetCurrentSong():MusicLengthSeconds()/songLength
+	local stream=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Stream')*GAMESTATE:GetCurrentSong():MusicLengthSeconds()/songLength
+	local radar_voltage=voltage-0.5
+	local radar_stream=stream-0.5
+	local bpms=Step:GetTimingData():GetActualBPM()
+	local tapspoint=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_TapsAndHolds')
+	tapspoint=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Jumps')/1.05+tapspoint
+	tapspoint=((radar_stream>=0) and radar_stream*66 or radar_stream*50)+tapspoint
+	tapspoint=((radar_voltage>=0) and radar_voltage*80 or radar_voltage*50)+tapspoint
+	tapspoint=math.max(tapspoint-666,0)*1.05+tapspoint
+	tapspoint=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Mines')/8+tapspoint
+	tapspoint=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Holds')/8+tapspoint
+	tapspoint=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Rolls')+tapspoint
+	tapspoint=Step:GetRadarValues(PLAYER_1):GetValue('RadarCategory_Chaos')*10+tapspoint
+	tapspoint=25+tapspoint
+	tapspoint=math.max(130-bpms[1],0)+tapspoint
+	tapspoint=math.max(math.min(bpms[2],400)-160,0)/5+tapspoint
+	tapspoint=math.round(tapspoint*52/songLength/5)
+	tapspoint=tapspoint*1.2
+	if songLength>300 then
+		tapspoint=tapspoint*1.14;
+	elseif songLength>150 then
+		tapspoint=tapspoint*1.09;
+	elseif songLength>120 then
+		tapspoint=tapspoint*1.05;
+	elseif songLength>100 then
+		tapspoint=tapspoint*1.02;
+	end;
+	--if songLength>60 then tapspoint=tapspoint*(1+((songLength-60)/30)*0.025) end
+	if tapspoint>=#tapsp2lv100*1.98 then
+		tapspoint=#tapsp2lv100
+	else
+		if tapspoint>=#tapsp2lv100 then
+			local max=#tapsp2lv100*1.98-#tapsp2lv100
+			tapspoint=#tapsp2lv100*0.85+(#tapsp2lv100*0.15*math.sin((math.pi*0.5)*(tapspoint-#tapsp2lv100)/max))
+		elseif tapspoint>=#tapsp2lv100*0.5 then
+			local max=#tapsp2lv100-#tapsp2lv100*0.5
+			tapspoint=#tapsp2lv100*0.5+(#tapsp2lv100*0.35*math.sin((math.pi*0.5)*(tapspoint-#tapsp2lv100*0.5)/max))
+		end
+	end
+	tapspoint=math.round(tapspoint)
+	tapspoint=math.max(tapspoint+1,1)
+	tapspoint=math.min(tapspoint,#tapsp2lv100)
+
+	local meter
+	if tapsp2lv100[tapspoint]<25 then
+		meter=math.round(12*math.sin(tapsp2lv100[tapspoint]*math.pi*0.5/25))
+	elseif tapsp2lv100[tapspoint]<35 then
+		meter=math.round((tapsp2lv100[tapspoint]-25)*3/10+12)
+	else
+		meter=math.round((tapsp2lv100[tapspoint]-35)*5/15+15)
+	end
+	return meter
 end
