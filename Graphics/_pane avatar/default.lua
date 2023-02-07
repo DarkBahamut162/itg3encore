2b970f39b3c775c6e5f541792c4a4b18c6804829
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-02-07 18:03:57 +0100

    Finally fixed...

--- Graphics/_pane avatar/default.lua
+++ Graphics/_pane avatar/default.lua
@@ -5,7 +5,7 @@ return Def.ActorFrame{
 	Condition=isWidescreen() and isOutFox(),
 	InitCommand=function(self) self:y(-1) end,
 	Def.Sprite{
-		InitCommand=function(self) self:xy(-96,110):zoomto(58,58):SetTexture(LoadModule("Options.GetProfileData.lua")(player)["Image"]) end,
+		InitCommand=function(self) self:xy(-96,110):CropTo(58,58):Load(LoadModule("Options.GetProfileData.lua")(player)["Image"]) end,
 		OnCommand=function(self) self:addx(player == PLAYER_1 and -SCREEN_CENTER_X or SCREEN_CENTER_X):sleep(0.4):decelerate(0.3):addx(player == PLAYER_1 and SCREEN_CENTER_X or -SCREEN_CENTER_X) end,
 		OffCommand=function(self) self:linear(0.4):diffusealpha(0) end
 	},
