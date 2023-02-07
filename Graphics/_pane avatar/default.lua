2b2b2950753ec67d442054fdd67ed1add5cca65c
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-02-07 18:15:36 +0100

    Forgot something for Project OutFox Player Avatar

--- Graphics/_pane avatar/default.lua
+++ Graphics/_pane avatar/default.lua
@@ -5,13 +5,13 @@ return Def.ActorFrame{
 	Condition=isWidescreen() and isOutFox(),
 	InitCommand=function(self) self:y(-1) end,
 	Def.Sprite{
-		InitCommand=function(self) self:xy(-96,110):CropTo(58,58):Load(LoadModule("Options.GetProfileData.lua")(player)["Image"]) end,
+		InitCommand=function(self) self:xy(player == PLAYER_1 and -96 or 96,110):CropTo(58,58):Load(LoadModule("Options.GetProfileData.lua")(player)["Image"]) end,
 		OnCommand=function(self) self:addx(player == PLAYER_1 and -SCREEN_CENTER_X or SCREEN_CENTER_X):sleep(0.4):decelerate(0.3):addx(player == PLAYER_1 and SCREEN_CENTER_X or -SCREEN_CENTER_X) end,
 		OffCommand=function(self) self:linear(0.4):diffusealpha(0) end
 	},
 	Def.ActorFrame{
 		LoadActor("border")..{
-			InitCommand=function(self) self:xy(-96,110):shadowlength(1):zoom(0.8) end,
+			InitCommand=function(self) self:xy(player == PLAYER_1 and -96 or 96,110):shadowlength(1):zoom(0.8) end,
 			OnCommand=function(self) self:addx(player == PLAYER_1 and -SCREEN_CENTER_X or SCREEN_CENTER_X):sleep(0.4):decelerate(0.3):addx(player == PLAYER_1 and SCREEN_CENTER_X or -SCREEN_CENTER_X) end,
 			OffCommand=function(self) self:linear(0.4):diffusealpha(0) end
 		}
