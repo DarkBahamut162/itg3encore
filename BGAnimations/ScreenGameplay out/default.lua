4c0857c59ab27b82a3e98ef2bc7f67ab4ba59b65
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-02-16 10:29:34 +0100

    Round Cleared graphics upgraded for final encore

--- BGAnimations/ScreenGameplay out/default.lua
+++ BGAnimations/ScreenGameplay out/default.lua
@@ -5,17 +5,17 @@ return Def.ActorFrame{
 			StartTransitioningCommand=function(self) self:queuecommand("Check"):linear(0.3):diffusealpha(1):sleep(1) end,
 			CheckCommand=function(self) if AnyPlayerFullComboed() then SOUND:PlayOnce( THEME:GetPathS( '', "FullComboSplash" ) ) self:hibernate(3) end end
 		},
-		LoadActor("_round")..{
+		LoadActor("_round "..(isFinal() and "final" or "normal"))..{
 			InitCommand=function(self) self:CenterX():y(SCREEN_CENTER_Y-75):zoom(0.6):diffusealpha(0):addy(-30) end,
 			StartTransitioningCommand=function(self) self:queuecommand("Check"):sleep(0.3):linear(0.3):diffusealpha(1):addy(30) end,
 			CheckCommand=function(self) if AnyPlayerFullComboed() then self:hibernate(3) end end
 		},
-		LoadActor("_cleared bottom")..{
+		LoadActor("_cleared bottom "..(isFinal() and "final" or "normal"))..{
 			InitCommand=function(self) self:CenterX():y(SCREEN_CENTER_Y+15):zoom(0.9):diffusealpha(0):addx(100) end,
 			StartTransitioningCommand=function(self) self:queuecommand("Check"):sleep(0.6):decelerate(0.3):diffusealpha(1):addx(-100):sleep(0.5) end,
 			CheckCommand=function(self) if AnyPlayerFullComboed() then self:hibernate(3) end end
 		},
-		LoadActor("_cleared top")..{
+		LoadActor("_cleared top "..(isFinal() and "final" or "normal"))..{
 			InitCommand=function(self) self:CenterX():y(SCREEN_CENTER_Y+10):zoom(0.9):blend(Blend.Add):diffusealpha(0):addx(-100) end,
 			StartTransitioningCommand=function(self) self:queuecommand("Check"):sleep(0.6):decelerate(0.3):diffusealpha(1):addx(100):sleep(0.5) end,
 			CheckCommand=function(self) if AnyPlayerFullComboed() then self:hibernate(3) end end
