d72959add9d8cdee4c497076d141fc45b531337f
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-02-28 12:17:18 +0100

    Actually do the correct flash

--- BGAnimations/ScreenAttract cancel.lua
+++ BGAnimations/ScreenAttract cancel.lua
@@ -3,13 +3,13 @@ local t = Def.ActorFrame{
 		InitCommand=function(self) self:diffuse(color("#000000")):stretchto(SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM) end,
 		OnCommand=function(self) self:diffusealpha(0):linear(0.5):diffusealpha(1):sleep(0.5) end
 	},
-	LoadActor("updatecoin") .. {
+	LoadActor("_flash") .. {
 		InitCommand=function(self) self:Center():blend(Blend.Add) end,
-		OnCommand=function(self) self:zoom(0):diffusealpha(0):rotationz(-20):linear(0.5):zoom(18):diffusealpha(1):rotationz(150) end
+		OnCommand=function(self) self:zoom(0):diffusealpha(0):rotationz(-20):linear(0.5):zoom(8*GetScreenAspectRatio()):diffusealpha(1):rotationz(40) end
 	},
-	LoadActor("updatecoin") .. {
+	LoadActor("_flash") .. {
 		InitCommand=function(self) self:Center():blend(Blend.Add) end,
-		OnCommand=function(self) self:zoom(0):diffusealpha(0):rotationz(20):linear(0.5):zoom(18):diffusealpha(1):rotationz(-70) end
+		OnCommand=function(self) self:zoom(0):diffusealpha(0):rotationz(20):linear(0.5):zoom(8*GetScreenAspectRatio()):diffusealpha(1):rotationz(-40) end
 	}
 }
 
