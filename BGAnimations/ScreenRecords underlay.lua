9960e90d200a45150d5e6178dac2c4fd6b260c7e
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-03-03 12:26:22 +0100

    Forgot the normal/final graphics switch here

--- BGAnimations/ScreenRecords underlay.lua
+++ BGAnimations/ScreenRecords underlay.lua
@@ -10,18 +10,27 @@ return Def.ActorFrame{
 		AnimCommand=function(self) self:croptop(-0.8):cropbottom(1):fadebottom(0.45):fadetop(0.45):linear(3):croptop(1):cropbottom(-0.8):sleep(1):queuecommand("Anim") end
 	},
 	LoadActor(THEME:GetPathB("ScreenTitleMenu","background/_lside"))..{
+		Condition=not isFinal(),
 		InitCommand=function(self) self:x(SCREEN_LEFT):y(SCREEN_BOTTOM+100):halign(0):valign(1) end,
-		OnCommand=function(self) self:decelerate(0.4):y(SCREEN_BOTTOM) end,
+		OnCommand=function(self) self:decelerate(0.8):y(SCREEN_BOTTOM) end,
 		OffCommand=function(self) self:accelerate(0.5):addy(100) end
 	},
 	LoadActor(THEME:GetPathB("ScreenTitleMenu","background/_lside"))..{
+		Condition=not isFinal(),
 		InitCommand=function(self) self:x(SCREEN_RIGHT):y(SCREEN_BOTTOM+100):halign(0):valign(1):zoomx(-1) end,
-		OnCommand=function(self) self:decelerate(0.4):y(SCREEN_BOTTOM) end,
+		OnCommand=function(self) self:decelerate(0.8):y(SCREEN_BOTTOM) end,
 		OffCommand=function(self) self:accelerate(0.5):addy(100) end
 	},
 	LoadActor(THEME:GetPathB("ScreenTitleMenu","background/width"))..{
+		Condition=not isFinal(),
 		InitCommand=function(self) self:x(SCREEN_LEFT+48):y(SCREEN_BOTTOM+100):horizalign(left):vertalign(bottom):zoomtowidth(SCREEN_WIDTH-96) end,
-		OnCommand=function(self) self:decelerate(0.4):y(SCREEN_BOTTOM) end,
+		OnCommand=function(self) self:decelerate(0.8):y(SCREEN_BOTTOM) end,
+		OffCommand=function(self) self:accelerate(0.5):addy(100) end
+	},
+	LoadActor(THEME:GetPathB("ScreenTitleMenu","background/base final"))..{
+		Condition=isFinal(),
+		InitCommand=function(self) self:CenterX():y(SCREEN_BOTTOM+100):valign(1):zoomtowidth(SCREEN_WIDTH) end,
+		OnCommand=function(self) self:decelerate(0.8):y(SCREEN_BOTTOM) end,
 		OffCommand=function(self) self:accelerate(0.5):addy(100) end
 	},
 	LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_normaltop")),
