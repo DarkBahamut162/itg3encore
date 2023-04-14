7d813b77d366645f3bea2aabfb59fa83b0fac0e1
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-04-14 11:14:53 +0200

    I whoopsed the ScreenEvaluationSummary BannerFrame

--- BGAnimations/ScreenEvaluationSummary decorations.lua
+++ BGAnimations/ScreenEvaluationSummary decorations.lua
@@ -1,8 +1,7 @@
 return Def.ActorFrame{
     LoadFallbackB(),
     StandardDecorationFromFileOptional("BannerList","BannerList"),
-    Def.Sprite{
-        Texture= THEME:GetPathG("ScreenEvaluation","BannerFrame"),
+    LoadActor(THEME:GetPathG("ScreenEvaluation","BannerFrame"))..{
         InitCommand=function(self) self:CenterX():y(SCREEN_CENTER_Y+135*WideScreenDiff()):zoom(WideScreenDiff()) end,
         OnCommand=function(self) self:diffusealpha(0):sleep(3):linear(0.5):diffusealpha(1) end,
         OffCommand=function(self) self:stoptweening():linear(0.2):diffusealpha(0) end
