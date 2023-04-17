481716ce9ad11a0490f65ade4598b5b0e8bb3b88
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-04-17 18:40:25 +0200

    WideScreenDiff() shouldn't be applied here

--- Graphics/ScreenEvaluation StageAward.lua
+++ Graphics/ScreenEvaluation StageAward.lua
@@ -20,11 +20,11 @@ local t = Def.ActorFrame{}
 if award and not STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetFailed() then
 	t[#t+1] = LoadActor(awardRef[award].File,awardRef[award].Color)..{
 		Name="Trophy",
-		InitCommand=function(self) self:zoom(0.7*WideScreenDiff()):x(-60*WideScreenDiff()):y(-80*WideScreenDiff()):rotationy(-15) end
+		InitCommand=function(self) self:zoom(0.7):x(-60):y(-80):rotationy(-15) end
 	}
 	t[#t+1] = LoadFont("_eurostile normal")..{
 		Name="Text",
-		InitCommand=function(self) self:zoom(WideScreenDiff()):halign(1):shadowlength(2):maxwidth(220):settext(THEME:GetString( "StageAward", ToEnumShortString(award) )) end
+		InitCommand=function(self) self:halign(1):shadowlength(2*WideScreenDiff()):maxwidth(220):settext(THEME:GetString( "StageAward", ToEnumShortString(award) )) end
 	}
 end
 
