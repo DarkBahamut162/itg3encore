6bb586819b5946e1f9c7c4cd555029de36c4c8f4
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-04-24 17:33:22 +0200

    Forgot to force save the ThemePrefs Setting

--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -1,6 +1,14 @@
 InitOptions()
+InitUserPrefs()
 
 return Def.ActorFrame{
+	OnCommand=function(self)
+		if not FILEMAN:DoesFileExist("Save/ThemePrefs.ini") then
+			Trace("ThemePrefs doesn't exist; creating file")
+			ThemePrefs.ForceSave()
+		end
+		ThemePrefs.Save()
+	end,
 	LoadActor(THEME:GetPathB("ScreenEndingNormal","overlay/p1gradient"))..{
 		InitCommand=function(self) self:x(SCREEN_RIGHT):y(SCREEN_TOP+63*WideScreenDiff()):halign(1):valign(0):zoomtoheight(SCREEN_HEIGHT):addx(150*WideScreenDiff()) end,
 		OnCommand=function(self) self:sleep(0.8):decelerate(0.2):addx(-150*WideScreenDiff()) end
