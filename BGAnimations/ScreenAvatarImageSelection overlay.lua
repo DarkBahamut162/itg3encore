6c38dc65462991360ede5c240176fa1575c45f62
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-05-17 10:59:35 +0200

    Add & Adjust OutFox Profile/Avatar Customization

---
+++ BGAnimations/ScreenAvatarImageSelection overlay.lua
@@ -0,0 +1,176 @@
+local curitem = 1
+local profile_location = "Save/LocalProfiles/".. GAMESTATE:GetEditLocalProfileID() .."/OutFoxPrefs.ini"
+local maximum_array_size = math.floor( SCREEN_WIDTH / 96 / 0.75)
+local img_cache = LoadModule("Profile.GetAvatarImageArray.lua")()
+local imgsave = LoadModule("Config.Load.lua")("AvatarImage", profile_location )
+local yarrayamm = (#img_cache / maximum_array_size)
+local menu_start = SCREEN_CENTER_Y*WideScreenSemiDiff()
+local spacingyscale = 14
+local spacingxscale = 71.6
+local lastX = 0
+local lastY = 0
+local curX = 0
+local curY = 0
+
+local t = Def.ActorFrame{
+    ChangeItemsMessageCommand=function(self,param)
+        local c = param.item
+        local Sel = self:GetChild("Selector")
+        local CImg = self:GetChild("InfoFrame"):GetChild("CurImage")
+        local CTex = self:GetChild("InfoFrame"):GetChild("CurText")
+
+        lastX = curX
+        lastY = curY
+
+        curX = 50*WideScreenSemiDiff()+math.mod( c-1, maximum_array_size )*spacingxscale
+        curY = menu_start+(
+            math.mod( c > maximum_array_size*6 and maximum_array_size*5 or c-1, maximum_array_size*6)
+            -
+            math.mod( c > maximum_array_size*6 and maximum_array_size*5 or c-1, maximum_array_size)
+        )*spacingyscale
+
+        Sel:stoptweening():decelerate(0.1):xy(curX,curY)
+        CImg:Load( img_cache[c][2] ):setsize(96,96)
+        CTex:settext( string.gsub( img_cache[c][1], ".png", "" ) )
+        if lastX ~= curX or lastY ~= curY then
+            MESSAGEMAN:Broadcast("Next")
+        end
+    end
+}
+
+local ImgGrid = Def.ActorFrame{
+    ScrollGridMessageCommand=function(self,param) self:stoptweening():decelerate(0.2):y( -80*(param.tileoffset-5) ) end
+}
+
+if #img_cache > 0 then
+    for i,v in pairs( img_cache ) do
+        -- Look if we have an existing image with the listing.
+        if imgsave and tostring(imgsave) == v[2] then curitem = i end
+        ImgGrid[#ImgGrid+1] = Def.ActorFrame{
+            OnCommand=function(self)
+                self:xy( 50*WideScreenSemiDiff()+math.mod( i-1, maximum_array_size )*spacingxscale,
+                menu_start+( math.mod(i-1, maximum_array_size*yarrayamm)-math.mod(i-1, maximum_array_size) )*spacingyscale )
+            end,
+            Def.Sprite{
+                Texture = v[2],
+                OnCommand=function(self)self:setsize(64,64):diffusealpha(0):addy(20):sleep(0.004*i):decelerate(0.1):addy(-20):diffusealpha(1) end,
+                OffCommand=function(self) self:stoptweening():sleep( 0.004*i):accelerate(0.1):addy(20):diffusealpha(0) end,
+            },
+        }
+    end
+    t[#t+1] = ImgGrid
+end
+
+t[#t+1] = Def.Quad{
+    Name = "Selector",
+    Condition = #img_cache > 0,
+    OnCommand=function(self)
+        self:zoomto(64,64):diffuseshift():effectcolor1( color("#FFFFFF99") ):effectcolor2( color("#FFFFFF00") ):xy(
+            50*WideScreenSemiDiff()+math.mod( curitem-1, maximum_array_size )*spacingxscale,
+            menu_start+( math.mod(curitem-1, maximum_array_size*yarrayamm)-math.mod(curitem-1, maximum_array_size) )*spacingyscale
+        ):diffusealpha(0):sleep(0.24):linear(0.1):diffusealpha(1)
+    end,
+    OffCommand=function(self) self:linear(0.2):diffusealpha(0) end
+}
+
+t[#t+1] = Def.ActorFrame{
+    Name = "InfoFrame",
+    InitCommand=function(self) self:zoom(WideScreenDiff()) end,
+    OnCommand=function(self) self:diffusealpha(0):addy(-20):decelerate(0.2):addy(20):diffusealpha(1) end,
+    OffCommand=function(self) self:accelerate(0.2):addy(-20):diffusealpha(0) end,
+    Def.BitmapText{
+        Font = "Common Normal",
+        InitCommand=function(self) self:settext( "Before" ) end,
+        OnCommand=function(self) self:xy( 38, 136-60 ):zoom(0.75):halign(0) end
+    },
+    Def.Sprite{
+        Name = "SetImage",
+        InitCommand=function(self) self:Load( imgsave ) end,
+        OnCommand=function(self) self:xy( 38, 136 ):halign(0):setsize(96,96) end
+    },
+    Def.BitmapText{
+        Font = "Common Normal",
+        InitCommand=function(self) self:settext( "=>" ) end,
+        OnCommand=function(self) self:xy( 48+96, 136 ):zoom(0.75):halign(0) end
+    },
+    Def.BitmapText{
+        Font = "Common Normal",
+        InitCommand=function(self) self:settext( "After" ) end,
+        OnCommand=function(self) self:xy( 38+140, 136-60 ):zoom(0.75):halign(0) end
+    },
+    Def.Sprite{
+        Condition = #img_cache > 0,
+        Name = "CurImage",
+        InitCommand=function(self) self:Load( img_cache[curitem][2] ) end,
+        OnCommand=function(self) self:xy( 38+140, 136 ):halign(0):setsize(96,96) end
+    },
+    Def.BitmapText{
+        Condition = #img_cache > 0,
+        Name = "CurText",
+        Font = "Common Normal",
+        InitCommand=function(self) self:settext( string.gsub( img_cache[curitem][1], ".(^)", "" ) ) end,
+        OnCommand=function(self) self:xy( 50+236, 136 ):zoom(0.75):halign(0) end
+    }
+}
+
+t[#t+1] = Def.BitmapText{
+    Condition = #img_cache < 1,
+    Name = "CurText",
+    Font = "Common Normal",
+    Text = "There are no avatars loaded on your Appearance folder.\nGo to Appearance/Avatars and drop some images in.",
+    OnCommand=function(self) self:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y):zoom(WideScreenDiff()) end
+}
+
+local ChangeInput = function(offset)
+    curitem = curitem + offset
+    if curitem < 1 then curitem = 1 end
+    if curitem > #img_cache then curitem = #img_cache end
+    MESSAGEMAN:Broadcast("ChangeItems",{item=curitem})
+    MESSAGEMAN:Broadcast("ScrollGrid",{tileoffset=curitem > maximum_array_size*6 and math.floor( (curitem-1)/maximum_array_size) or 5 })
+end
+
+local Inputs = {
+    ["MenuLeft"] = function() ChangeInput(-1) end,
+    ["MenuDown"] = function() ChangeInput(maximum_array_size) end,
+    ["MenuUp"] = function() ChangeInput(-maximum_array_size) end,
+    ["MenuRight"] = function() ChangeInput(1) end,
+    ["Start"] = function()
+        if #img_cache > 0 then
+            LoadModule("Config.Save.lua")( "AvatarImage", img_cache[curitem][2], profile_location )
+        end
+        SCREENMAN:PlayStartSound()
+        SCREENMAN:GetTopScreen():StartTransitioningScreen( "SM_GoToNextScreen", 0 )
+    end,
+    ["Back"] = function()
+        SCREENMAN:PlayCancelSound()
+        SCREENMAN:GetTopScreen():StartTransitioningScreen( "SM_GoToPrevScreen", 0 )
+    end
+}
+
+local Controller = function(event)
+    if event.type == "InputEventType_FirstPress" then
+        if Inputs[event.GameButton] then
+            Inputs[event.GameButton]()
+        end
+    end
+end
+
+t.OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback( Controller ) end
+
+t[#t+1] = Def.Sound{
+	File = THEME:GetPathS("ScreenOptions","change"),
+	IsAction = true,
+	ChangeMessageCommand=function(self) self:play() end
+}
+
+t[#t+1] = Def.Sound{
+	File = THEME:GetPathS("ScreenOptions","next"),
+	IsAction = true,
+	NextMessageCommand=function(self) self:play() end
+}
+
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_sides"))
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_base"))
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_normaltop"))
+
+return t
---
+++ BGAnimations/ScreenOptionsCustomizeProfile overlay.lua
@@ -0,0 +1,475 @@
+-- Pester Kyzentun for an explanation if you need to customize this screen.
+-- Also, this might be rewritten to us a proper customizable lua menu system
+-- in the future.
+
+-- Copy this file into your theme, then modify as needed to suit your theme.
+-- Each of the things on this list has a comment marking it, so you can
+-- quickly find it by searching.
+-- Things you will want to change:
+-- 1.  The Numpad
+-- 2.  The Cursor
+-- 3.  The Menu Items
+-- 4.  The Menu Values
+-- 4.1  The L/R indicators
+-- 5.  The Menu Fader
+
+local profile = GAMESTATE:GetEditLocalProfile()
+local master = GAMESTATE:GetMasterPlayerNumber()
+local profile_id = GAMESTATE:GetEditLocalProfileID()
+
+-- 1.  The Numpad
+-- This is what sets up how the numpad looks.  See Scripts/04 NumPadEntry.lua
+-- for a full description of how to customize a NumPad.
+-- Note that if you provide a custom prompt actor for the NumPad, it must
+-- have a SetCommand because the NumPad is used any time the player needs to
+-- enter a number, and the prompt is updated by running its SetCommand.
+local number_entry = new_numpad_entry{
+	Name = "number_entry",
+	InitCommand=function(self) self:diffusealpha(0):xy(_screen.cx, _screen.cy):zoom(WideScreenDiff()) end,
+	InvalidValueCommand=function(self) SCREENMAN:PlayInvalidSound() end,
+	EntryDoneCommand=function(self) SCREENMAN:PlayStartSound() end,
+	value_color = PlayerColor(master),
+	cursor_draw = "first",
+	cursor_color = PlayerDarkColor(master)
+}
+
+local function calc_list_pos(value, list)
+	for i, entry in ipairs(list) do
+		if entry.setting == value then return i end
+	end
+	return 1
+end
+
+local function item_value_to_text(item, value)
+	if item.item_type == "bool" then
+		if value then
+			value = THEME:GetString("ScreenOptionsCustomizeProfile", item.true_text)
+		else
+			value = THEME:GetString("ScreenOptionsCustomizeProfile", item.false_text)
+		end
+	elseif item.item_type == "list" then
+		local pos = calc_list_pos(value, item.list)
+		return item.list[pos].display_name
+	end
+	return value
+end
+
+local char_list = {}
+
+do
+	local all_chars = CHARMAN:GetAllCharacters()
+	for i, char in ipairs(char_list) do
+		char_list[#char_list+1] = {
+			setting = char:GetCharacterID(),
+			display_name = char:GetDisplayName()
+		}
+	end
+end
+
+local menu_items = {
+	{
+		name = "weight",
+		get = "GetWeightPounds",
+		set = "SetWeightPounds",
+		item_type = "number",
+		auto_done = 10
+	},{
+		name = "voomax",
+		get = "GetVoomax",
+		set = "SetVoomax",
+		item_type = "number",
+		auto_done = 10
+	},{
+		name = "birth_year",
+		get = "GetBirthYear",
+		set = "SetBirthYear",
+		item_type = "number",
+		auto_done = 1000
+	},{
+		name = "calorie_calc",
+		get = "GetIgnoreStepCountCalories",
+		set = "SetIgnoreStepCountCalories",
+		item_type = "bool",
+		true_text = "use_heart",
+		false_text = "use_steps"
+	},{
+		name = "gender",
+		get = "GetIsMale",
+		set = "SetIsMale",
+		item_type = "bool",
+		true_text = "male",
+		false_text = "female"
+	}
+}
+if #char_list > 0 then
+	menu_items[#menu_items+1] = {
+		name = "character",
+		get = "GetCharacter",
+		set = "SetCharacter",
+		item_type = "list",
+		list = char_list
+	}
+end
+if isOutFox() then
+	menu_items[#menu_items+1] = {
+		name = "avatimg_edit",
+		item_type = "menu",
+		get = function()
+			local ProfileImage = LoadModule("Config.Load.lua")( "AvatarImage", "/Save/LocalProfiles/"..profile_id.."/OutFoxPrefs.ini" )
+			return ProfileImage and string.gsub(ProfileImage, "/Appearance/Avatars/","") or "default"
+		end,
+		screen_name = "ScreenAvatarImageSelection"
+	}
+end
+menu_items[#menu_items+1] = {
+	name = "exit",
+	item_type = "exit"
+}
+
+local menu_pos = 1
+local menu_start = (SCREEN_CENTER_Y-30)*WideScreenSemiDiff()
+local menu_x = 100*WideScreenDiff()
+local value_x = SCREEN_RIGHT-500*WideScreenDiff()
+local fader
+local cursor_on_menu = "main"
+local menu_item_actors = {}
+local menu_values = {}
+local list_pos = 0
+local active_list = {}
+local left_showing = false
+local right_showing = false
+local sfx
+
+local function fade_actor_to(actor, alf)
+	actor:stoptweening():linear(0.2):diffusealpha(alf)
+end
+
+local function update_menu_cursor()
+	MESSAGEMAN:Broadcast("UpdateCursor",{ind=menu_pos})
+end
+
+local function update_list_cursor()
+	local valactor = menu_values[menu_pos]
+	valactor:playcommand("Set", {active_list[list_pos].display_name})
+	if list_pos > 1 then
+		if not left_showing then
+			valactor:playcommand("ShowLeft")
+			left_showing = true
+		end
+	else
+		if left_showing then
+			valactor:playcommand("HideLeft")
+			left_showing = false
+		end
+	end
+	if list_pos < #active_list then
+		if not right_showing then
+			valactor:playcommand("ShowRight")
+			right_showing = true
+		end
+	else
+		if right_showing then
+			valactor:playcommand("HideRight")
+			right_showing = false
+		end
+	end
+end
+
+local function exit_screen(newscreen)
+	PROFILEMAN:SaveLocalProfile(profile_id)
+	if newscreen then
+		SCREENMAN:GetTopScreen():SetNextScreenName( newscreen )
+	end
+	SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+	SCREENMAN:PlayStartSound()
+end
+
+local function input(event)
+	local pn = event.PlayerNumber
+	if not pn then return false end
+	if event.type == "InputEventType_Release" then return false end
+	local button = event.GameButton
+	if cursor_on_menu == "main" then
+		if button == "Start" then
+			local item= menu_items[menu_pos]
+			if item.item_type == "bool" then
+				local value = not profile[item.get](profile)
+				menu_values[menu_pos]:playcommand("Set", {item_value_to_text(item, value)})
+				profile[item.set](profile, value)
+				MESSAGEMAN:Broadcast("Change")
+			elseif item.item_type == "number" then
+				fade_actor_to(fader, .8)
+				fade_actor_to(number_entry.container, 1)
+				number_entry.value = profile[item.get](profile)
+				number_entry.value_actor:playcommand("Set", {number_entry.value})
+				number_entry.auto_done_value = item.auto_done
+				number_entry.max_value = item.max
+				number_entry:update_cursor(number_entry.cursor_start)
+				number_entry.prompt_actor:playcommand("Set", {THEME:GetString("ScreenOptionsCustomizeProfile", item.name)})
+				cursor_on_menu = "numpad"
+				SCREENMAN:PlayStartSound()
+			elseif item.item_type == "list" then
+				cursor_on_menu = "list"
+				active_list = menu_items[menu_pos].list
+				list_pos = calc_list_pos(profile[menu_items[menu_pos].get](profile), active_list)
+				update_list_cursor()
+			elseif item.item_type == "menu" then
+				exit_screen( item.screen_name )
+			elseif item.item_type == "exit" then
+				exit_screen()
+			end
+		elseif button == "Back" then
+			exit_screen()
+		else
+			if button == "MenuLeft" or button == "MenuUp" then
+				if menu_pos > 1 then
+					menu_pos = menu_pos - 1
+					update_menu_cursor()
+					MESSAGEMAN:Broadcast("Next")
+				else
+					SCREENMAN:PlayInvalidSound()
+				end
+			elseif button == "MenuRight" or button == "MenuDown" then
+				if menu_pos < #menu_items then
+					menu_pos = menu_pos + 1
+					update_menu_cursor()
+					MESSAGEMAN:Broadcast("Next")
+				else
+					SCREENMAN:PlayInvalidSound()
+				end
+			end
+		end
+	elseif cursor_on_menu == "numpad" then
+		local done= number_entry:handle_input(button)
+		if done or button == "Back" then
+			local item= menu_items[menu_pos]
+			if button ~= "Back" then
+				profile[item.set](profile, number_entry.value)
+				menu_values[menu_pos]:playcommand("Set", {item_value_to_text(item, number_entry.value)})
+			else
+				SCREENMAN:PlayCancelSound()
+			end
+			fade_actor_to(fader, 0)
+			fade_actor_to(number_entry.container, 0)
+			cursor_on_menu = "main"
+		elseif button == "Start" then
+			MESSAGEMAN:Broadcast("Change")
+		elseif button == "MenuLeft" or button == "MenuUp" or button == "MenuRight" or button == "MenuDown" then
+			MESSAGEMAN:Broadcast("Next")
+		end
+	elseif cursor_on_menu == "list" then
+		if button == "MenuLeft" or button == "MenuUp" then
+			if list_pos > 1 then list_pos = list_pos - 1 end
+			update_list_cursor()
+			menu_values[menu_pos]:playcommand("PressLeft")
+		elseif button == "MenuRight" or button == "MenuDown" then
+			if list_pos < #active_list then list_pos = list_pos + 1 end
+			update_list_cursor()
+			menu_values[menu_pos]:playcommand("PressRight")
+		elseif button == "Start" or button == "Back" then
+			if button ~= "Back" then
+				profile[menu_items[menu_pos].set](profile, active_list[list_pos].setting)
+			end
+			local valactor = menu_values[menu_pos]
+			left_showing = false
+			right_showing = false
+			valactor:playcommand("HideLeft")
+			valactor:playcommand("HideRight")
+			cursor_on_menu = "main"
+		end
+	end
+end
+
+local args = {
+	Def.Actor{
+		OnCommand=function(self)
+			MESSAGEMAN:Broadcast("UpdateCursor",{ind=1})
+			SCREENMAN:GetTopScreen():AddInputCallback(input)
+		end
+	}
+}
+
+-- Note that the "character" item in the menu only shows up if there are
+-- characters to choose from.  You might want to adjust positioning for that.
+local itemspacing = 56/2
+
+for i, item in ipairs(menu_items) do
+	local item_y = menu_start + ((i-1) * itemspacing)
+	-- 3.  The Menu Items
+	-- This creates the actor that will be used to show each item on the menu.
+	local menuitemnew
+	if item.get then
+		local value_text = type(item.get) == "string" and item_value_to_text(item, profile[item.get](profile)) or item.get()
+		-- 4.  The Menu Values
+		-- Each of the values needs to have a SetCommand so it can be updated
+		-- when the player changes it.
+		-- And ActorFrame is used because values for list items need to have
+		-- left/right indicators for when the player is making a choice.
+		local value_args = {
+			Name = "value_" .. item.name,
+			InitCommand=function(self)
+				-- Note that the ActorFrame is being added to the list menu_values
+				-- so it can be easily fetched and updated when the value changes.
+				menu_values[i] = self
+				self:x(value_x):diffusealpha(0):linear(0.2):diffusealpha(1)
+			end,
+			OffCommand=function(self) self:linear(0.2):diffusealpha(0) end,
+			Def.BitmapText{
+				Name = "val",
+				Font = "Common Normal",
+				Text = value_text,
+				InitCommand=function(self) self:diffuse(Color.White):zoom(0.75*WideScreenDiff()):horizalign(left) end,
+				SetCommand=function(self, param) self:settext(param[1]) end
+			}
+		}
+		if item.item_type == "list" then
+			-- 4.1  The L/R indicators
+			-- The L/R indicators are there to tell the player when there is a
+			-- choice to the left or right of the choice they are on.
+			-- Note that they are placed inside the ActorFrame for the value, so
+			-- when commands are played on the ActorFrame, they are played for the
+			-- indicators too.
+			-- The commands are ShowLeft, HideLeft, PressLeft, and the same for
+			-- Right.
+			-- Note that the right indicator has a SetCommand so it sees when the
+			-- value changes and checks the new width to position itself.
+			-- Show/Hide is only played when the indicator changes state.
+			-- Command execution order: Set, Show/Hide (if change occurred), Press
+			value_args[#value_args+1] = Def.ActorMultiVertex{
+				InitCommand= function(self)
+					self:SetVertices{
+						{{-5, 0, 0}, Color.White},
+						{{0, -10, 0}, Color.White},
+						{{0, 10, 0}, Color.White}
+					}:SetDrawState{Mode = "DrawMode_Triangles"}:x(-8):visible(false):playcommand("Set", {value_text})
+				end,
+				ShowLeftCommand=function(self) self:visible(true) end,
+				HideLeftCommand=function(self) self:visible(false) end,
+				PressLeftCommand=function(self) self:stoptweening():linear(0.2):zoom(2):linear(0.2):zoom(1) end
+			}
+			value_args[#value_args+1] = Def.ActorMultiVertex{
+				InitCommand=function(self)
+					self:SetVertices{
+						{{5, 0, 0}, Color.White},
+						{{0, -10, 0}, Color.White},
+						{{0, 10, 0}, Color.White}
+					}:SetDrawState{Mode = "DrawMode_Triangles"}:visible(false)
+				end,
+				SetCommand=function(self)
+					local valw = self:GetParent():GetChild("val"):GetWidth()
+					self:x(valw+8)
+				end,
+				ShowRightCommand=function(self) self:visible(true) end,
+				HideRightCommand=function(self) self:visible(false) end,
+				PressRightCommand=function(self) self:stoptweening():linear(0.2):zoom(2):linear(0.2):zoom(1) end
+			}
+		end
+		menuitemnew = Def.ActorFrame(value_args)
+	end
+
+	args[#args+1] = Def.ActorFrame{
+		InitCommand=function(self) self:y(item_y) end,
+		UpdateCursorMessageCommand=function(self) self:stoptweening():smooth(0.2):y( menu_pos > 8 and item_y-(itemspacing*(menu_pos-8)) or item_y ) end,
+		Def.Quad{
+			OnCommand=function(self) self:x( 0 ):halign(0):zoomto( SCREEN_WIDTH, itemspacing-2 ):diffusealpha(0):linear(0.2):diffuse( color("#00000076") ) end,
+			OffCommand=function(self) self:linear(0.2):diffusealpha(0) end
+		},
+		Def.ActorFrame{
+			InitCommand=function(self) self:diffusealpha(0) end,
+			OffCommand=function(self) self:linear(0.2):diffusealpha(0) end,
+			UpdateCursorMessageCommand=function(self,param) self:stoptweening():linear(0.16):diffusealpha(param.ind == i and 1 or 0) end,
+			Def.Quad {
+				InitCommand=function(self)
+					self:x(0):faderight( 0.5 ):zoomto(SCREEN_WIDTH, 2 ):halign(0):vertalign(top):y(-itemspacing/2):diffuse(PlayerColor(master)):diffuseleftedge(ColorLightTone(PlayerColor(master)))
+				end
+			},
+			Def.Quad {
+				InitCommand=function(self)
+					self:x(0):faderight( 0.5 ):zoomto(SCREEN_WIDTH, 2 ):halign(0):vertalign(bottom):y(itemspacing/2):diffuse(ColorLightTone(PlayerColor(master))):diffuseleftedge(PlayerColor(master))
+				end
+			}
+		},
+
+		Def.BitmapText{
+			Name = "menu_" .. item.name,
+			Font = "Common Normal",
+			Text = THEME:GetString("ScreenOptionsCustomizeProfile", item.name),
+			InitCommand=function(self)
+				-- Note that the item adds itself to the list menu_item_actors.  This
+				-- is so that when the cursor is moved, the appropriate item can be
+				-- easily fetched for positioning and sizing the cursor.
+				-- Note the ActorFrames have a width of 1 unless you set it, so when
+				-- you change this from an BitmapText to a ActorFrame, you will have
+				-- to make the FitCommand of your cursor look at the children.
+				menu_item_actors[i] = self
+				self:x(menu_x*WideScreenDiff()):diffuse(Color.White):horizalign(left):zoom(0.75*WideScreenDiff()):diffusealpha(0):linear(0.2):diffusealpha(1)
+			end,
+			UpdateCursorMessageCommand=function(self,param) self:stoptweening():linear(0.16):diffusealpha(param.ind == i and 1 or 0.6) end,
+			OffCommand=function(self) self:linear(0.2):diffusealpha(0) end
+		},
+		menuitemnew
+	}
+end
+
+local AvI = isOutFox() and LoadModule("Config.Load.lua")( "AvatarImage", "/Save/LocalProfiles/"..profile_id.."/OutFoxPrefs.ini" ) or false
+local ProfileImage = AvI and AvI or THEME:GetPathG("UserProfile","generic icon")
+
+args[#args+1] = Def.ActorFrame{
+    Name = "InfoFrame",
+	InitCommand=function(self) self:zoom(WideScreenDiff()) end,
+    OnCommand=function(self) self:diffusealpha(0):addy(-20):decelerate(0.2):addy(20):diffusealpha(1) end,
+    OffCommand=function(self) self:accelerate(0.2):addy(-20):diffusealpha(0) end,
+    Def.Sprite{
+		Condition=isOutFox(),
+        InitCommand=function(self) self:Load( ProfileImage ) end,
+        OnCommand=function(self) self:xy( 38, 136 ):halign(0):setsize(96,96) end
+    },
+	Def.BitmapText{
+		Font = "Common Normal",
+		Text = "Profile: "..profile:GetDisplayName(),
+		OnCommand=function(self) self:xy( isOutFox() and 150 or 50, 100 ):halign(0) end
+	},
+	Def.BitmapText{
+		Font = "Common Normal",
+		Text = "GUID: "..profile:GetGUID(),
+		OnCommand=function(self) self:xy( isOutFox() and 150 or 50, 136 ):halign(0) end
+	},
+	Def.BitmapText{
+		Font = "Common Normal",
+		Text = Screen.String("TotalTime").. ": "..SecondsToHHMMSS(profile:GetTotalGameplaySeconds()),
+		OnCommand=function(self) self:xy( isOutFox() and 150 or 50, 172 ):halign(0) end
+	}
+}
+
+-- 5.  The Menu Fader
+-- This is just something to tell the player that the menu is no longer
+-- active because they are interacting with the numpad.
+-- Default is to just fade this in over the top of the menu.  If you want
+-- something different, change the places in the input function that call
+-- fade_actor_to to do what you want with the fader.
+args[#args+1]= Def.Quad{
+	Name = "fader",
+	InitCommand=function(self)
+		fader = self
+		self:setsize( SCREEN_WIDTH ,SCREEN_HEIGHT):Center(menu_x-10, menu_start-12):diffuse(Color.Black):diffusealpha(0)
+	end
+}
+
+args[#args+1] = Def.Sound{
+	File = THEME:GetPathS("ScreenOptions","change"),
+	IsAction = true,
+	ChangeMessageCommand=function(self) self:play() end
+}
+
+args[#args+1] = Def.Sound{
+	File = THEME:GetPathS("ScreenOptions","next"),
+	IsAction = true,
+	NextMessageCommand=function(self) self:play() end
+}
+
+args[#args+1] = number_entry:create_actors()
+args[#args+1] = LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_sides"))
+args[#args+1] = LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_base"))
+args[#args+1] = LoadActor(THEME:GetPathB("ScreenWithMenuElements","underlay/_normaltop"))
+
+return Def.ActorFrame(args)
