f26fd21301095fde1c3b0abdc3d8782fc99b5451
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-07-10 11:06:13 +0200

    Show total group time & adjust if over 99:59:99

--- Graphics/ScreenSelectMusic SongTime.lua
+++ Graphics/ScreenSelectMusic SongTime.lua
@@ -1,9 +1,14 @@
+local c
+
 return Def.ActorFrame{
+	BeginCommand = function(self) c = self:GetChildren() end,
 	LoadFont("titlemenu")..{
+		Name="Time",
 		Text="Time:",
 		InitCommand=function(self) self:x(-103):zoom(1.05):halign(1) end
 	},
 	LoadFont("_r bold bevel numbers")..{
+		InitCommand=function(self) self:x(98):halign(1) end,
 		SetCommand=function(self)
 			local curSelection = nil
 			local length = 0.0
@@ -19,8 +24,14 @@ return Def.ActorFrame{
 				curSelection = GAMESTATE:GetCurrentSong()
 				if curSelection then
 					length = curSelection:MusicLengthSeconds()
+				else
+					local songs = SONGMAN:GetSongsInGroup(SCREENMAN:GetTopScreen():GetMusicWheel():GetSelectedSection())
+					for s=1,#songs do
+						length = length + songs[s]:MusicLengthSeconds()
+					end
 				end
 			end
+			if length >= 6000 then c.Time:x(-131) else c.Time:x(-103) end
 			self:settext( SecondsToMMSSMsMs(length) )
 		end,
 		CurrentSongChangedMessageCommand=function(self) self:queuecommand("Set") end,
