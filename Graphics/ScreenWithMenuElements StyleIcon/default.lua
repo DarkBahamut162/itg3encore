313fa21ab0fafe44b017ffcb4a1235f432975d73
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-07-19 12:52:42 +0200

    This had been wrong the whole time?

--- Graphics/ScreenWithMenuElements StyleIcon/default.lua
+++ Graphics/ScreenWithMenuElements StyleIcon/default.lua
@@ -1,18 +1,14 @@
-local style = GAMESTATE:GetCurrentStyle()
-if style then
-	local master_pn = GAMESTATE:GetMasterPlayerNumber()
-	local st = style:GetName()
-	local state = 0
+local master_pn = GAMESTATE:GetMasterPlayerNumber()
+local st = ""
+local state = 0
 
-	if st == "Single" then
-		state = (master_pn == PLAYER_2) and 1 or 0
-	else
-		st = "Versus"
-	end
-
-	return LoadActor(THEME:GetPathG("ScreenWithMenuElements","StyleIcon/"..st))..{
-		InitCommand=function(self) self:animate(false):setstate(state) end
-	}
+if GAMESTATE:GetNumPlayersEnabled() == 1 then
+	st = "Single"
+	state = (master_pn == PLAYER_2) and 1 or 0
 else
-	return Def.Actor{}
+	st = "Versus"
 end
+
+return LoadActor(THEME:GetPathG("ScreenWithMenuElements","StyleIcon/"..st))..{
+	InitCommand=function(self) self:animate(false):setstate(state) end
+}
