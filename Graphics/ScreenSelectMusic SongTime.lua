05d79b5822a39dcca2fc343fed5d183a230821de
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-07-24 13:47:15 +0200

    Show difference between Music-&StepTime

--- Graphics/ScreenSelectMusic SongTime.lua
+++ Graphics/ScreenSelectMusic SongTime.lua
@@ -1,5 +1,6 @@
 local c
 local stepsType = StepsTypeSingle()[GetUserPrefN("StylePosition")]
+
 return Def.ActorFrame{
 	BeginCommand = function(self) c = self:GetChildren() end,
 	LoadFont("titlemenu")..{
@@ -11,32 +12,43 @@ return Def.ActorFrame{
 		InitCommand=function(self) self:x(98):halign(1) end,
 		SetCommand=function(self)
 			local curSelection = nil
-			local length = 0.0
+			local length = {0.0,0.0}
 			if GAMESTATE:IsCourseMode() then
 				curSelection = GAMESTATE:GetCurrentCourse()
 				if curSelection then
 					local trail = GAMESTATE:GetCurrentTrail(GAMESTATE:GetMasterPlayerNumber())
 					if trail then
-						length = TrailUtil.GetTotalSeconds(trail)
+						length[1] = TrailUtil.GetTotalSeconds(trail)
 					end
 				end
 			else
 				curSelection = GAMESTATE:GetCurrentSong()
 				if curSelection then
-					length = curSelection:MusicLengthSeconds()
+					length[1] = curSelection:MusicLengthSeconds()
+					length[2] = length[2] + curSelection:GetLastSecond()-curSelection:GetFirstSecond()
 				else
 					local songs = SONGMAN:GetSongsInGroup(SCREENMAN:GetTopScreen():GetMusicWheel():GetSelectedSection())
 					for s=1,#songs do
 						if songs[s]:HasStepsType(stepsType) then
-							length = length + songs[s]:MusicLengthSeconds()
+							length[1] = length[1] + songs[s]:MusicLengthSeconds()
+							length[2] = length[2] + songs[s]:GetLastSecond()-songs[s]:GetFirstSecond()
 						end
 					end
 				end
+				MESSAGEMAN:Broadcast('SetTimeP1', {Time = length[2]})
 			end
-			if length >= 6000 then c.Time:x(-131) else c.Time:x(-103) end
-			self:settext( SecondsToMMSSMsMs(length) )
+			if length[1] >= 6000 then c.Time:x(-131) else c.Time:x(-103) end
+			self:settext( SecondsToMMSSMsMs(length[1]) )
 		end,
 		CurrentSongChangedMessageCommand=function(self) self:queuecommand("Set") end,
 		CurrentCourseChangedMessageCommand=function(self) self:queuecommand("Set") end
+	},
+	LoadFont("_r bold bevel numbers")..{
+		Condition=not GAMESTATE:IsCourseMode(),
+		Name="P1",
+		InitCommand=function(self)
+			self:x(98):y(-30):halign(1):diffuseshift():effectcolor1(PlayerColor(GAMESTATE:GetMasterPlayerNumber())):effectcolor2(color("#FFFFFF")):effectclock("beat")
+		end,
+		SetTimeP1MessageCommand=function(self,param) self:settext(SecondsToMMSSMsMs(param.Time)) end
 	}
 }
