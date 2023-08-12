bc74c9bfd1166b6b38b628b0794f6b16d38aad3d
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-08-12 11:56:34 +0200

    Why to I keep on forgetting the little things...

--- Graphics/ScreenEnding Card p2/default.lua
+++ Graphics/ScreenEnding Card p2/default.lua
@@ -1,5 +1,5 @@
 return Def.ActorFrame{
-	LoadActor("card "..(isFinal() and "Final" or "Normal")..{
+	LoadActor("card "..(isFinal() and "Final" or "Normal"))..{
 		InitCommand=function(self) self:glowshift():zoom(0.7) end,
 		CardRemovedP2MessageCommand=function(self) self:diffusealpha(0) end
 	}
