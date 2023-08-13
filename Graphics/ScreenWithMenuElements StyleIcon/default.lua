d1f66a1e4b14e60af7a815f2c857c2535bf2a279
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-08-13 15:16:50 +0200

    Have doubleres instead of halving the zoom

--- Graphics/ScreenWithMenuElements StyleIcon/default.lua
+++ Graphics/ScreenWithMenuElements StyleIcon/default.lua
@@ -10,12 +10,12 @@ if GameModeEnabled() and style then
 	return Def.ActorFrame{
 		LoadActor(THEME:GetPathG("ScreenWithMenuElements","StyleIcon/"..stepsType.."/"..GetUserPrefN("StylePosition")))..{
 			InitCommand=function(self)
-				self:zoom(0.5):addy(-3):shadowcolor(color("#000000")):shadowlengthx(0):shadowlengthy(2):diffusealpha(1/3)
+				self:addy(-3):shadowcolor(color("#000000")):shadowlengthx(0):shadowlengthy(2):diffusealpha(1/3)
 			end
 		},
 		LoadActor(THEME:GetPathG("ScreenWithMenuElements","StyleIcon/"..stepsType.."/"..GetUserPrefN("StylePosition")))..{
 			InitCommand=function(self)
-				self:zoom(0.5):addy(-3):shadowcolor(color("#000000")):shadowlengthx(0):shadowlengthy(2)
+				self:addy(-3):shadowcolor(color("#000000")):shadowlengthx(0):shadowlengthy(2)
 				if not players[PLAYER_1] then self:cropleft(0.5) end
 				if not players[PLAYER_2] then self:cropright(0.5) end
 			end
