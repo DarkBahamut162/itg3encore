410c9f7dc66e1299b7ef1815633dc4da97619a59
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-11-10 17:34:23 +0100

    I noticed and did it, too!

--- Scripts/Colors.lua
+++ Scripts/Colors.lua
@@ -1,66 +1,78 @@
 function PlayerColor( pn )
-	if pn == PLAYER_1 then return color("#FFDE00") end
-	if pn == PLAYER_2 then return color("#14FF00") end
-	return color("1,1,1,1")
+	local colors = {
+		[PLAYER_1] = color("#FFDE00"),
+		[PLAYER_2] = color("#14FF00")
+	}
+	return colors[pn] or color("1,1,1,1")
 end
 
 function PlayerColorSemi( pn )
-	if pn == PLAYER_1 then return color("#FFDE0080") end
-	if pn == PLAYER_2 then return color("#14FF0080") end
-	return color("1,1,1,0.5")
+	local colors = {
+		[PLAYER_1] = color("#FFDE0080"),
+		[PLAYER_2] = color("#14FF0080")
+	}
+	return colors[pn] or color("1,1,1,0.5")
 end
 
 function CustomDifficultyToColor( dc )
-	if dc == "Beginner"		then return color("#D05CF6") end
-	if dc == "Easy"			then return color("#09FF10") end
-	if dc == "Medium"		then return color("#F3F312") end
-	if dc == "Hard"			then return color("#EA3548") end
-	if dc == "Challenge"	then return color("#16AFF3") end
-	if dc == "Edit"			then return color("#F7F7F7") end
+	local colors = {
+		["Beginner"]	= color("#D05CF6"),
+		["Easy"]		= color("#09FF10"),
+		["Medium"]		= color("#F3F312"),
+		["Hard"]		= color("#EA3548"),
+		["Challenge"]	= color("#16AFF3"),
+		["Edit"]		= color("#F7F7F7"),
 
-	if dc == "Freestyle"	then return color("#09FF10") end
-	if dc == "HalfDouble"	then return color("#F3F312") end
-	if dc == "Crazy"		then return color("#EA3548") end
-	if dc == "Nightmare"	then return color("#16AFF3") end
-	return color("1,1,1,1")
+		["Freestyle"]	= color("#09FF10"),
+		["HalfDouble"]	= color("#F3F312"),
+		["Crazy"]		= color("#EA3548"),
+		["Nightmare"]	= color("#16AFF3")
+	}
+	return colors[dc] or color("1,1,1,1")
 end
 
 function ContrastingDifficultyColor( dc )
-	if dc == "Beginner"		then return color("#E2ABF5") end
-	if dc == "Easy"			then return color("#B2FFB5") end
-	if dc == "Medium"		then return color("#F2F2AA") end
-	if dc == "Hard"			then return color("#EBA4AB") end
-	if dc == "Challenge"	then return color("#AADCF2") end
-	if dc == "Edit"			then return color("#F7F7F7") end
+	local colors = {
+		["Beginner"]	= color("#E2ABF5"),
+		["Easy"]		= color("#B2FFB5"),
+		["Medium"]		= color("#F2F2AA"),
+		["Hard"]		= color("#EBA4AB"),
+		["Challenge"]	= color("#AADCF2"),
+		["Edit"]		= color("#F7F7F7"),
 
-	if dc == "Freestyle"	then return color("#B2FFB5") end
-	if dc == "HalfDouble"	then return color("#F2F2AA") end
-	if dc == "Crazy"		then return color("#EBA4AB") end
-	if dc == "Nightmare"	then return color("#AADCF2") end
-	return color("1,1,1,1")
+		["Freestyle"]	= color("#B2FFB5"),
+		["HalfDouble"]	= color("#F2F2AA"),
+		["Crazy"]		= color("#EBA4AB"),
+		["Nightmare"]	= color("#AADCF2")
+	}
+	return colors[dc] or color("1,1,1,1")
 end
 
 function DifficultyToColor( dc )
-	if dc == 'Difficulty_Beginner'		then return CustomDifficultyToColor("Beginner") end
-	if dc == 'Difficulty_Easy'			then return CustomDifficultyToColor("Easy") end
-	if dc == 'Difficulty_Medium'		then return CustomDifficultyToColor("Medium") end
-	if dc == 'Difficulty_Hard'			then return CustomDifficultyToColor("Hard") end
-	if dc == 'Difficulty_Challenge'		then return CustomDifficultyToColor("Challenge") end
-	if dc == 'Difficulty_Edit'			then return CustomDifficultyToColor("Edit") end
+	local colors = {
+		["Difficulty_Beginner"]	= CustomDifficultyToColor("Beginner"),
+		["Difficulty_Easy"]		= CustomDifficultyToColor("Easy"),
+		["Difficulty_Medium"]		= CustomDifficultyToColor("Medium"),
+		["Difficulty_Hard"]		= CustomDifficultyToColor("Hard"),
+		["Difficulty_Challenge"]	= CustomDifficultyToColor("Challenge"),
+		["Difficulty_Edit"]		= CustomDifficultyToColor("Edit"),
 
-	if dc == 'Difficulty_Freestyle'		then return CustomDifficultyToColor("Easy") end
-	if dc == 'Difficulty_HalfDouble'	then return CustomDifficultyToColor("Medium") end
-	if dc == 'Difficulty_Crazy'			then return CustomDifficultyToColor("Hard") end
-	if dc == 'Difficulty_Nightmare'		then return CustomDifficultyToColor("Challenge") end
-	return color("1,1,1,1")
+		["Difficulty_Freestyle"]	= CustomDifficultyToColor("Freestyle"),
+		["Difficulty_HalfDouble"]	= CustomDifficultyToColor("HalfDouble"),
+		["Difficulty_Crazy"]		= CustomDifficultyToColor("Crazy"),
+		["Difficulty_Nightmare"]	= CustomDifficultyToColor("Nightmare")
+	}
+	return colors[dc] or color("1,1,1,1")
 end
 
 function TapNoteScoreToColor( tns )
-	if tns == "TapNoteScore_W1"		then return color("#7BE8FF") end
-	if tns == "TapNoteScore_W2"		then return color("#FFA959") end
-	if tns == "TapNoteScore_W3"		then return color("#67FF19") end
-	if tns == "TapNoteScore_W4"		then return color("#D366FF") end
-	if tns == "TapNoteScore_W5"		then return color("#FF7149") end
-	if tns == "TapNoteScore_Miss"	then return color("#FF0808") end
-	return color("1,1,1,1")
+	local colors = {
+		["TapNoteScore_W1"]		= color("#7BE8FF"),
+		["TapNoteScore_W2"]		= color("#FFA959"),
+		["TapNoteScore_W3"]		= color("#67FF19"),
+		["TapNoteScore_W4"]		= color("#D366FF"),
+		["TapNoteScore_W5"]		= color("#FF7149"),
+		["TapNoteScore_Miss"]	= color("#FF0808")
+	}
+	return colors[dc] or color("1,1,1,1")
 end
