3e7cb6637671c21d3c38e9d4377074ed29c9fd83
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-11-22 08:58:01 +0100

    Save at all times FFS

--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -1,12 +1,5 @@
 return Def.ActorFrame{
 	BeginCommand=function() InitOptions() end,
-	OnCommand=function(self)
-		if not FILEMAN:DoesFileExist("Save/ThemePrefs.ini") then
-			Trace("ThemePrefs doesn't exist; creating file")
-			ThemePrefs.ForceSave()
-		end
-		ThemePrefs.Save()
-	end,
 	LoadActor(THEME:GetPathB("ScreenEndingNormal","overlay/p1gradient"))..{
 		InitCommand=function(self) self:x(SCREEN_RIGHT):y(SCREEN_TOP+63*WideScreenDiff()):halign(1):valign(0):zoomtoheight(SCREEN_HEIGHT):addx(150*WideScreenDiff()) end,
 		OnCommand=function(self) self:sleep(0.8):decelerate(0.2):addx(-150*WideScreenDiff()) end
