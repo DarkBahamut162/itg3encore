85b2e2b4d5244032c58eaccf0c4b4d697e471c66
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-12-20 14:33:14 +0100

    Move BPMCalc into SelMusic.lua & clean up CourseHasMods.lua

--- Graphics/ScreenSelectMusic CourseHasMods.lua
+++ Graphics/ScreenSelectMusic CourseHasMods.lua
@@ -57,99 +57,19 @@ return Def.ActorFrame{
 							end
 						end
 						if ThemePrefs.Get("ShowTrueBPMs") then -- Get true BPM range
-							local timingdata = curStep:GetTimingData()
-							local bpms = curStep:GetDisplayBpms()
-							local truebpms = timingdata:GetActualBPM()
-
-							bpms[1] = math.round(bpms[1] * 1000) / 1000
-							bpms[2] = math.round(bpms[2] * 1000) / 1000
-							truebpms[1] = math.round(truebpms[1] * 1000) / 1000
-							truebpms[2] = math.round(truebpms[2] * 1000) / 1000
-
-							for i=1,2 do
-								if bpms[i] then if math.abs(1-bpms[i]/math.round(bpms[i])) < 0.005 then bpms[i] = math.round(bpms[i]) end end
-								if truebpms[i] then if math.abs(1-truebpms[i]/math.round(truebpms[i])) < 0.005 then truebpms[i] = math.round(truebpms[i]) end end
-							end
-
-							if bpms[1] == truebpms[1] and bpms[2] == truebpms[2] and bpms[1] == bpms[2] then
-								output = addToOutput(output,"BPM: "..truebpms[1],"\n")
+							local bpms = getTrueBPMs(curSelection,curStep)
+							if bpms[1] == bpms[2] and bpms[2] == bpms[3] then
+								output = addToOutput(output,"BPM: "..bpms[1],"\n")
+							elseif bpms[3] == bpms[1] or bpms[3] == 0 then
+								if bpms[1] ~= bpms[2] then
+									output = addToOutput(output,"BPM: "..bpms[1] .. " (" .. bpms[2] .. ")","\n")
+								else
+									output = addToOutput(output,"BPM: "..bpms[1],"\n")
+								end
+							elseif bpms[3] < bpms[2] then
+								output = addToOutput(output,"BPM: "..bpms[1] .. " - " .. bpms[3] .. " (" .. bpms[2] .. ")","\n")
 							else
-								local sets = timingdata:GetBPMsAndTimes()
-								local currentSet, lastSet
-								local BPMs, duration, lastDuration = {}, 0, 0
-								local fastestBPM, fastestBPM_backup = 0, 0
-
-								for i, set in ipairs(sets) do
-									currentSet = split("=",set)
-									currentSet[2]=math.round(currentSet[2] * 1000 / 1000)
-
-									if lastSet then
-										duration = (currentSet[1]-lastSet[1]) / lastSet[2] * 60
-										if BPMs[lastSet[2]] then
-											BPMs[lastSet[2]] = BPMs[lastSet[2]] + duration
-										else
-											BPMs[lastSet[2]] = duration
-										end
-										if math.abs(1-lastSet[2]/currentSet[2]) <= 0.02 then
-											duration = duration + lastDuration
-											if truebpms[1] <= currentSet[2] and truebpms[2] >= currentSet[2] then
-												if fastestBPM < currentSet[2] then fastestBPM = currentSet[2] end
-											end
-										end
-										if duration >= 6 then
-											if truebpms[1] <= lastSet[2] and truebpms[2] >= lastSet[2] then
-												if fastestBPM < lastSet[2] then fastestBPM = lastSet[2] end
-											end
-											if truebpms[1] <= currentSet[2] and truebpms[2] >= currentSet[2] then
-												if math.abs(1-lastSet[2]/currentSet[2]) <= 0.02 then
-													if fastestBPM < currentSet[2] then fastestBPM = currentSet[2] end
-												end
-											end
-										end
-									end
-									lastSet, lastDuration = currentSet, duration
-								end
-
-								duration = (curSelection:GetLastBeat()-lastSet[1]) / lastSet[2] * 60
-								if BPMs[lastSet[2]] then
-									BPMs[lastSet[2]] = BPMs[lastSet[2]] + duration
-								else
-									BPMs[lastSet[2]] = duration
-								end
-								if duration >= 4 then
-									if truebpms[1] <= lastSet[2] and truebpms[2] >= lastSet[2] then
-										if fastestBPM < lastSet[2] then fastestBPM = lastSet[2] end
-									end
-								end
-								if math.abs(1-fastestBPM/truebpms[2]) <= 0.04 then fastestBPM = truebpms[2] end
-								--[[
-								local function pairsByKeys (t, f)
-									local a = {}
-									for n in pairs(t) do table.insert(a, n) end
-									table.sort(a, f)
-									local i = 0
-									local iter = function()
-										i = i + 1
-										if a[i] == nil then return nil
-										else return a[i], t[ a[i] ]
-										end
-									end
-									return iter
-								end
-								for _bpm, _seconds in pairsByKeys(BPMs) do if _seconds >= 10 then fastestBPM_backup = _bpm end end
-								if fastestBPM_backup > fastestBPM then fastestBPM = fastestBPM_backup end
-								]]--
-								if fastestBPM == truebpms[1] or fastestBPM == 0 then
-									if truebpms[1] ~= truebpms[2] then
-										output = addToOutput(output,"BPM: "..truebpms[1] .. " (" .. truebpms[2] .. ")","\n")
-									else
-										output = addToOutput(output,"BPM: "..truebpms[1],"\n")
-									end
-								elseif fastestBPM < truebpms[2] then
-									output = addToOutput(output,"BPM: "..truebpms[1] .. " - " .. fastestBPM .. " (" .. truebpms[2] .. ")","\n")
-								else
-									output = addToOutput(output,"BPM: "..truebpms[1] .. " - " .. truebpms[2],"\n")
-								end
+								output = addToOutput(output,"BPM: "..bpms[1] .. " - " .. bpms[2],"\n")
 							end
 						end
 						if ThemePrefs.Get("ShowCalcDiff") then --Calculate Difficulty
