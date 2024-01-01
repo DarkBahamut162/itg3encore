220ff0f95d996b9acacebf8fbfcabbd69605c009
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-01-01 16:35:31 +0100

    Okay... Doing it this way then...

--- BGAnimations/ScreenEvaluationNormal decorations.lua
+++ BGAnimations/ScreenEvaluationNormal decorations.lua
@@ -109,19 +109,19 @@ local function GraphDisplay(pn)
 		LoadActor(THEME:GetPathB("ScreenEvaluation","underlay/FGC "..pname(pn)))..{
 			Condition=not isRave() and getenv("EvalCombo"..pname(pn)) and lastPerfectSecond > 0 and not (isOni() and not isLifeline(player)),
 			InitCommand=function(self)
-				self:croptop(0.75) if lastGreatSecond ~= 0 then self:cropright(1-(lastGreatSecond/length)) end
+				self:croptop(0.78) if not STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):FullComboOfScore('TapNoteScore_W3') == true then self:cropright(1-(lastGreatSecond/length)) end
 			end
 		},
 		LoadActor(THEME:GetPathB("ScreenEvaluation","underlay/FEC "..pname(pn)))..{
 			Condition=not isRave() and getenv("EvalCombo"..pname(pn)) and lastMarvelousSecond > 0 and not (isOni() and not isLifeline(player)),
 			InitCommand=function(self)
-				self:croptop(0.75) if lastPerfectSecond ~= 0 then self:cropright(1-(lastPerfectSecond/length)) end
+				self:croptop(0.78) if not STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):FullComboOfScore('TapNoteScore_W2') == true then self:cropright(1-(lastPerfectSecond/length)) end
 			end
 		},
 		LoadActor(THEME:GetPathB("ScreenEvaluation","underlay/FFC "..pname(pn)))..{
 			Condition=not isRave() and getenv("EvalCombo"..pname(pn)) and not (isOni() and not isLifeline(player)),
 			InitCommand=function(self)
-				self:croptop(0.75) if lastMarvelousSecond ~= 0 then self:cropright(1-(lastMarvelousSecond/length)) end
+				self:croptop(0.78) if not STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):FullComboOfScore('TapNoteScore_W1') == true then self:cropright(1-(lastMarvelousSecond/length)) end
 			end
 		}
 	}
