3f97243ada5662622dab8a60cf9cce624b6f01e4
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-01-14 08:31:51 +0100

    Show Song Time differences if on 2Player & use StepCache'd TrueSeconds if on OutFox [AKA Fuck Song:GetLastSecond()]

--- Graphics/ScreenSelectMusic SongTime.lua
+++ Graphics/ScreenSelectMusic SongTime.lua
@@ -1,6 +1,9 @@
 local c
 local stepsType = StepsTypeSingle()[GetUserPrefN("StylePosition")]
 local course = GAMESTATE:IsCourseMode()
+local players = GAMESTATE:GetHumanPlayers()
+local length = {0.0,0.0,0.0}
+local EC = " "
 
 return Def.ActorFrame{
 	BeginCommand = function(self) c = self:GetChildren() end,
@@ -13,14 +16,24 @@ return Def.ActorFrame{
 		InitCommand=function(self) self:x(98):halign(1) end,
 		SetCommand=function(self)
 			local curSelection = nil
-			local length = {0.0,0.0}
-			local EC = " "
+			length = {0.0,0.0,0.0}
+			EC = " "
 			if GAMESTATE:IsCourseMode() then
 				curSelection = GAMESTATE:GetCurrentCourse()
 				if curSelection then
-					local trail = GAMESTATE:GetCurrentTrail(GAMESTATE:GetMasterPlayerNumber())
-					if trail then
-						length[1] = TrailUtil.GetTotalSeconds(trail)
+					for pos,player in pairs( players ) do
+						local trail = GAMESTATE:GetCurrentTrail(player)
+						if trail then
+							if length[1] == 0.0 then length[1] = TrailUtil.GetTotalSeconds(trail) end
+							local entries = trail:GetTrailEntries()
+							for i=1,#entries do
+								if isOutFox() then
+									length[pos+1] = length[pos+1] + LoadFromCache(entries[i]:GetSong(),entries[i]:GetSteps(),"TrueSeconds")
+								else
+									length[pos+1] = length[pos+1] + (entries[i]:GetSong():GetFirstSecond() > entries[i]:GetSong():GetLastSecond() and 0 or entries[i]:GetSong():GetLastSecond()-entries[i]:GetSong():GetFirstSecond())
+								end
+							end
+						end
 					end
 				end
 			else
@@ -28,7 +41,14 @@ return Def.ActorFrame{
 				if curSelection then
 					EC = curSelection:GetPreviewMusicPath()
 					length[1] = curSelection:MusicLengthSeconds()
-					length[2] = curSelection:GetFirstSecond() > curSelection:GetLastSecond() and 0 or curSelection:GetLastSecond()-curSelection:GetFirstSecond()
+					for pos,player in pairs( players ) do
+						if isOutFox() then
+							local steps = GAMESTATE:GetCurrentSteps(player)
+							if steps then length[pos+1] = LoadFromCache(curSelection,steps,"TrueSeconds") end
+						else
+							length[pos+1] = curSelection:GetFirstSecond() > curSelection:GetLastSecond() and 0 or curSelection:GetLastSecond()-curSelection:GetFirstSecond()
+						end
+					end
 				else
 					local songs = SONGMAN:GetSongsInGroup(SCREENMAN:GetTopScreen():GetMusicWheel():GetSelectedSection())
 					for s=1,#songs do
@@ -38,19 +58,42 @@ return Def.ActorFrame{
 						end
 					end
 				end
-				MESSAGEMAN:Broadcast('SetTime',{Time = length[2],EffectClock = EC ~= "" and "beat" or "timerglobal"})
 			end
+			MESSAGEMAN:Broadcast('SetTime')
 			if length[1] >= 6000 then c.Time:x(-131) else c.Time:x(-103) end
 			self:settext( SecondsToMMSSMsMs(length[1]) )
 		end,
 		CurrentSongChangedMessageCommand=function(self) if not course then self:queuecommand("Set") end end,
-		CurrentCourseChangedMessageCommand=function(self) if course then self:queuecommand("Set") end end
+		CurrentStepsP1ChangedMessageCommand=function(self) if not course then self:queuecommand("Set") end end,
+		CurrentStepsP2ChangedMessageCommand=function(self) if not course then self:queuecommand("Set") end end,
+		CurrentCourseChangedMessageCommand=function(self) if course then self:queuecommand("Set") end end,
+		CurrentTrailP1ChangedMessageCommand=function(self) if course then self:queuecommand("Set") end end,
+		CurrentTrailP2ChangedMessageCommand=function(self) if course then self:queuecommand("Set") end end
 	},
 	LoadFont("_r bold bevel numbers")..{
-		Condition=not GAMESTATE:IsCourseMode(),
 		InitCommand=function(self) self:x(98):y(-30):halign(1) end,
-		SetTimeMessageCommand=function(self,param)
-			self:settext(SecondsToMMSSMsMs(param.Time)):diffuseshift():effectcolor1(PlayerColor(GAMESTATE:GetMasterPlayerNumber())):effectcolor2(color("#FFFFFF")):effectclock(param.EffectClock)
+		SetTimeMessageCommand=function(self)
+			if length[3] ~= 0.0 then
+				local color1 = length[3] == length[2] and PlayerColor(PLAYER_1) or PlayerColor(PLAYER_2)
+				local color2 = length[3] == length[2] and PlayerColor(PLAYER_2) or color("#FFFFFF")
+				self:diffusealpha(1):settext(SecondsToMMSSMsMs(length[3])):diffuseshift():effectcolor1(color1):effectcolor2(color2):effectclock(EC ~= "" and "beat" or "timerglobal")
+			elseif length[2] ~= 0.0 then
+				local color1 = #players == 2 and PlayerColor(PLAYER_1) or PlayerColor(GAMESTATE:GetMasterPlayerNumber())
+				local color2 = #players == 2 and PlayerColor(PLAYER_2) or color("#FFFFFF")
+				self:diffusealpha(1):settext(SecondsToMMSSMsMs(length[2])):diffuseshift():effectcolor1(color1):effectcolor2(color2):effectclock(EC ~= "" and "beat" or "timerglobal")
+			else
+				self:diffusealpha(0)
+			end
+		end
+	},
+	LoadFont("_r bold bevel numbers")..{
+		InitCommand=function(self) self:x(98):y(-60):halign(1) end,
+		SetTimeMessageCommand=function(self)
+			if length[3] ~= 0.0 and length[2] ~= length[3] then
+				self:diffusealpha(1):settext(SecondsToMMSSMsMs(length[2])):diffuseshift():effectcolor1(PlayerColor(PLAYER_1)):effectcolor2(color("#FFFFFF")):effectclock(EC ~= "" and "beat" or "timerglobal")
+			else
+				self:diffusealpha(0)
+			end
 		end
 	}
 }
