d587e2fa57a7865df656c402ad1d810ec0831458
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-03-24 11:41:32 +0100

    Show if a song is favorited [OutFox V only]

--- Graphics/MusicWheelItem Song NormalPart.lua
+++ Graphics/MusicWheelItem Song NormalPart.lua
@@ -1,3 +1,21 @@
 return Def.ActorFrame{
-	LoadActor("MusicWheelItem _Song NormalPart"..(isFinal() and "Final" or "Normal"))
+	LoadActor("MusicWheelItem _Song NormalPart"..(isFinal() and "Final" or "Normal"))..{
+		SetMessageCommand=function(self,params)
+			if isOutFoxV() and params.Song then
+				local isFav = false
+
+				for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
+					if not isFav then 
+						isFav = PROFILEMAN:GetProfile(pn):SongIsFavorite(params.Song)
+					end
+				end
+
+				if isFav then
+					self:diffuseshift():effectcolor1(color("#FFFF00")):effectcolor2(color("#FFFFFF")):effectclock("beat")
+				else
+					self:stopeffect()
+				end
+			end
+		end
+	}
 }
