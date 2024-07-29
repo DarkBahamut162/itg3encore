51bb7fd08b42e95820b42f1c645abf52a60812b1
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-07-29 11:39:06 +0200

    Modify LyricCommand to add vertspacing for multiple lines

--- Scripts/ScreenGameplay.lua
+++ Scripts/ScreenGameplay.lua
@@ -150,3 +150,53 @@ function HoldEvenOdd(player)
 		return -1
 	end
 end
+
+function Actor:LyricCommand(side)
+	self:settext( Var "LyricText" )
+	self:draworder(102)
+	self:vertspacing(-10)
+
+	self:stoptweening()
+	self:shadowlengthx(0)
+	self:shadowlengthy(5)
+	self:strokecolor(color("#000000"))
+
+	local Zoom = SCREEN_WIDTH / (self:GetZoomedWidth()+1)
+	if( Zoom > 1 ) then
+		Zoom = 1
+	end
+	self:zoomx( Zoom )
+
+	local lyricColor = Var "LyricColor"
+	local Factor = 1
+	if side == "Back" then
+		Factor = 0.5
+	elseif side == "Front" then
+		Factor = 0.9
+	end
+	self:diffuse( {
+		lyricColor[1] * Factor,
+		lyricColor[2] * Factor,
+		lyricColor[3] * Factor,
+		lyricColor[4] * Factor } )
+
+	if side == "Front" then
+		self:cropright(1)
+	else
+		self:cropleft(0)
+	end
+
+	self:diffusealpha(0)
+	self:linear(0.2)
+	self:diffusealpha(0.75)
+	self:linear( Var "LyricDuration" * 0.75)
+	if side == "Front" then
+		self:cropright(0)
+	else
+		self:cropleft(1)
+	end
+	self:sleep( Var "LyricDuration" * 0.25 )
+	self:linear(0.2)
+	self:diffusealpha(0)
+	return self
+end
