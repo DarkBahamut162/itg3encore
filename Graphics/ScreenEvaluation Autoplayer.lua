b8a68bd95cc5e959bc8cf6aef07abc4dccc8e282
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-09-21 15:35:53 +0200

    Highscore is still possible during AutoPlay... Just adjust some things for that...

--- Graphics/ScreenEvaluation Autoplayer.lua
+++ Graphics/ScreenEvaluation Autoplayer.lua
@@ -1,11 +1,13 @@
 local player = ...
 assert(player,"[ScreenEvaluation AutoPlayer] requires player")
 local autoPlayer = getenv("EvalCombo"..pname(player))
+local record = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetPersonalHighScoreIndex()
+local hasPersonalRecord = (record == 0)
 
 return Def.ActorFrame{
 	LoadFont("_v 26px bold white")..{
 		Text="AutoPlayer",
-		InitCommand=function(self) self:zoomx(0.6):zoomy(0.5):shadowlength(2*WideScreenDiff()):cropright(1):visible(not autoPlayer) end,
+		InitCommand=function(self) self:y(hasPersonalRecord and 6 or 0):zoomx(0.6):zoomy(0.5):shadowlength(2*WideScreenDiff()):cropright(1):visible(not autoPlayer) end,
 		OnCommand=function(self) self:sleep(3):linear(0.3):cropright(0):diffuseshift():effectcolor1(color("#FF0000")) end
 	}
 }
