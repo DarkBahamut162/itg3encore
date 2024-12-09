c5534f385dd53417a821f28e5736c916713a5933
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-12-09 17:13:01 +0100

    Trying to fix this [ScreenMiniMenuContext] window

--- BGAnimations/_prompt background.lua
+++ BGAnimations/_prompt background.lua
@@ -1,6 +1,6 @@
 return Def.ActorFrame{
 	Def.Quad{
-		InitCommand=function(self) self:FullScreen():diffuse(color("0,0,0,0")) end,
+		InitCommand=function(self) self:zoomto(SCREEN_WIDTH*2,SCREEN_HEIGHT):diffuse(color("0,0,0,0")) end,
 		OnCommand=function(self) self:linear(0.3):diffusealpha(0.75) end,
 		OffCommand=function(self) self:linear(0.3):diffusealpha(0) end,
 		CancelCommand=function(self) self:linear(0.3):diffusealpha(0) end
