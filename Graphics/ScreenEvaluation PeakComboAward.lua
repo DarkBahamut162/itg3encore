71880f09ea6f94fa184d3e2620cc480d65397bce
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-12-25 13:40:11 +0100

    Only show PeakComboAward & StageAward when not in AutoPlay

--- Graphics/ScreenEvaluation PeakComboAward.lua
+++ Graphics/ScreenEvaluation PeakComboAward.lua
@@ -1,32 +1,34 @@
 local player = ...
 assert(player,"[ScreenEvaluation PeakComboAward] requires player")
 
-local awardRef = {
-	["PeakComboAward_1000"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "purple" },
-	["PeakComboAward_2000"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "red" },
-	["PeakComboAward_3000"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "orange" },
-	["PeakComboAward_4000"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "silver" },
-	["PeakComboAward_5000"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "gold" },
-	["PeakComboAward_6000"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "silver" },
-	["PeakComboAward_7000"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "gold" },
-	["PeakComboAward_8000"]		= { File = THEME:GetPathG("_award","models/itg"), Color = "silver" },
-	["PeakComboAward_9000"]		= { File = THEME:GetPathG("_award","models/itg"), Color = "gold" },
-	["PeakComboAward_10000"]	= { File = THEME:GetPathG("_award","models/itg"), Color = "blue" }
-}
-
 local t = Def.ActorFrame{}
-local award = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetPeakComboAward()
 
-if award then
-	t[#t+1] = loadfile(awardRef[award].File)(awardRef[award].Color)..{
-		Name="Trophy",
-		InitCommand=function(self) self:zoom(0.7):x(60):y(-80):rotationy(-15) end
+if getenv("EvalCombo"..pname(player)) then
+	local award = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetPeakComboAward()
+	local awardRef = {
+		["PeakComboAward_1000"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "purple" },
+		["PeakComboAward_2000"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "red" },
+		["PeakComboAward_3000"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "orange" },
+		["PeakComboAward_4000"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "silver" },
+		["PeakComboAward_5000"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "gold" },
+		["PeakComboAward_6000"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "silver" },
+		["PeakComboAward_7000"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "gold" },
+		["PeakComboAward_8000"]		= { File = THEME:GetPathG("_award","models/itg"), Color = "silver" },
+		["PeakComboAward_9000"]		= { File = THEME:GetPathG("_award","models/itg"), Color = "gold" },
+		["PeakComboAward_10000"]	= { File = THEME:GetPathG("_award","models/itg"), Color = "blue" }
 	}
-	t[#t+1] = Def.BitmapText {
-		File = "_eurostile normal",
-		Name="Combo",
-		InitCommand=function(self) self:halign(0):shadowlength(2*WideScreenDiff()):maxwidth(220):settext(THEME:GetString( "PeakComboAward", ToEnumShortString(award) ) ) end
-	}
+
+	if award then
+		t[#t+1] = loadfile(awardRef[award].File)(awardRef[award].Color)..{
+			Name="Trophy",
+			InitCommand=function(self) self:zoom(0.7):x(60):y(-80):rotationy(-15) end
+		}
+		t[#t+1] = Def.BitmapText {
+			File = "_eurostile normal",
+			Name="Combo",
+			InitCommand=function(self) self:halign(0):shadowlength(2*WideScreenDiff()):maxwidth(220):settext(THEME:GetString( "PeakComboAward", ToEnumShortString(award) ) ) end
+		}
+	end
 end
 
 return t
--- Graphics/ScreenEvaluation StageAward.lua
+++ Graphics/ScreenEvaluation StageAward.lua
@@ -1,32 +1,34 @@
 local player = ...
 assert(player,"[ScreenEvaluation StageAward] requires player")
-local award = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetStageAward()
-
-local awardRef = {
-	["StageAward_80PercentW3"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "green" },
-	["StageAward_90PercentW3"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "green" },
-	["StageAward_100PercentW3"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "green" },
-	["StageAward_SingleDigitW3"]	= { File = THEME:GetPathG("_award","models/ribbon"), Color = "pink" },
-	["StageAward_OneW3"]			= { File = THEME:GetPathG("_award","models/flag"), Color = "green" },
-	["StageAward_FullComboW3"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "blue" },
-	["StageAward_SingleDigitW2"]	= { File = THEME:GetPathG("_award","models/plaque"), Color = "silver" },
-	["StageAward_OneW2"]			= { File = THEME:GetPathG("_award","models/flag"), Color = "orange" },
-	["StageAward_FullComboW2"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "bronze" },
-	["StageAward_FullComboW1"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "bronze" }
-}
 
 local t = Def.ActorFrame{}
 
-if award and not STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetFailed() then
-	t[#t+1] = loadfile(awardRef[award].File)(awardRef[award].Color)..{
-		Name="Trophy",
-		InitCommand=function(self) self:zoom(0.7):x(-60):y(-80):rotationy(-15) end
+if getenv("EvalCombo"..pname(player)) then
+	local award = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetStageAward()
+	local awardRef = {
+		["StageAward_80PercentW3"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "green" },
+		["StageAward_90PercentW3"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "green" },
+		["StageAward_100PercentW3"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "green" },
+		["StageAward_SingleDigitW3"]	= { File = THEME:GetPathG("_award","models/ribbon"), Color = "pink" },
+		["StageAward_OneW3"]			= { File = THEME:GetPathG("_award","models/flag"), Color = "green" },
+		["StageAward_FullComboW3"]		= { File = THEME:GetPathG("_award","models/ribbon"), Color = "blue" },
+		["StageAward_SingleDigitW2"]	= { File = THEME:GetPathG("_award","models/plaque"), Color = "silver" },
+		["StageAward_OneW2"]			= { File = THEME:GetPathG("_award","models/flag"), Color = "orange" },
+		["StageAward_FullComboW2"]		= { File = THEME:GetPathG("_award","models/plaque"), Color = "bronze" },
+		["StageAward_FullComboW1"]		= { File = THEME:GetPathG("_award","models/trophy"), Color = "bronze" }
 	}
-	t[#t+1] = Def.BitmapText {
-		File = "_eurostile normal",
-		Name="Text",
-		InitCommand=function(self) self:halign(1):shadowlength(2*WideScreenDiff()):maxwidth(220):settext(THEME:GetString( "StageAward", ToEnumShortString(award) )) end
-	}
+
+	if award and not STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetFailed() then
+		t[#t+1] = loadfile(awardRef[award].File)(awardRef[award].Color)..{
+			Name="Trophy",
+			InitCommand=function(self) self:zoom(0.7):x(-60):y(-80):rotationy(-15) end
+		}
+		t[#t+1] = Def.BitmapText {
+			File = "_eurostile normal",
+			Name="Text",
+			InitCommand=function(self) self:halign(1):shadowlength(2*WideScreenDiff()):maxwidth(220):settext(THEME:GetString( "StageAward", ToEnumShortString(award) )) end
+		}
+	end
 end
 
 return t
