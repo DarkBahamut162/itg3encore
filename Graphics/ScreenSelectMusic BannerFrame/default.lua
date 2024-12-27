c57744070e76bd633de720e2b2cbb3570bff06e0
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-12-27 14:04:30 +0100

    Exchange Quickstart Border with something more noticeable on Light Mode

--- Graphics/ScreenSelectMusic BannerFrame/default.lua
+++ Graphics/ScreenSelectMusic BannerFrame/default.lua
@@ -9,33 +9,35 @@ return Def.ActorFrame{
 			Texture = "frame "..(isFinal() and "final" or "normal"),
 			InitCommand=function(self) self:x(SCREEN_CENTER_X+140*WideScreenDiff()):y(SCREEN_CENTER_Y-18*WideScreenDiff()):zoom(WideScreenDiff()) end
 		},
+		Def.ActorFrame{
+			Def.Sprite {
+				Texture = "border red",
+				InitCommand=function(self) self:x(SCREEN_CENTER_X+140*WideScreenDiff()):y(SCREEN_CENTER_Y-18*WideScreenDiff()):zoom(WideScreenDiff()) end,
+				OnCommand=function(self) self:playcommand("Blink") end,
+				BlinkCommand=function(self)
+					self:diffuseshift():effectcolor1(color("#00000000")):effectcolor2(color("#00000000"))
+					local song = GAMESTATE:GetCurrentSong()
+					if song then
+						local spmp = song:GetPreviewMusicPath()
+						local effectclock = spmp ~= "" and "beat" or "timerglobal"
+						if song:GetFirstSecond() <= 1 then
+							self:effectcolor1(color("#FFFFFFFF")):effectclock(effectclock):effectcolor2(color("#FFFFFFFF"))
+						elseif song:GetFirstSecond() <= 2 then
+							self:effectcolor1(color("#FFFFFFFF")):effectclock(effectclock):effectcolor2(color("#FFFFFF80"))
+						else
+							self:effectcolor1(color("#00000000")):effectclock(effectclock):effectcolor2(color("#00000000"))
+						end
+					end
+				end,
+				CurrentSongChangedMessageCommand=function(self) self:playcommand("Blink") end
+			}
+		},
 		Def.Sprite {
 			Texture = "flare",
 			InitCommand=function(self) self:x(SCREEN_CENTER_X+142*WideScreenDiff()):y(SCREEN_CENTER_Y-18*WideScreenDiff()):blend(Blend.Add) end,
 			OnCommand=function(self) self:diffusealpha(0) end,
 			RefreshCommand=function(self) self:stoptweening(5):zoom(WideScreenDiff()):diffusealpha(1):sleep(0.1):accelerate(0.2):zoom(1.2*WideScreenDiff()):diffusealpha(0) end,
 			CurrentSongChangedMessageCommand=function(self) if initialLoad then self:playcommand("Refresh") else initialLoad = true end end
-		},
-		Def.Sprite {
-			Texture = "flare",
-			InitCommand=function(self) self:x(SCREEN_CENTER_X+142*WideScreenDiff()):y(SCREEN_CENTER_Y-18*WideScreenDiff()):zoom(WideScreenDiff()):blend(Blend.Add) end,
-			OnCommand=function(self) self:playcommand("Blink") end,
-			BlinkCommand=function(self)
-				self:diffuseshift():effectcolor2(color("#000000"))
-				local song = GAMESTATE:GetCurrentSong()
-				if song then
-					local spmp = song:GetPreviewMusicPath()
-					local effectclock = spmp ~= "" and "beat" or "timerglobal"
-					if song:GetFirstSecond() <= 1 then
-						self:effectcolor1(color("#FF0000")):effectclock(effectclock):effectcolor2(color("#000000"))
-					elseif song:GetFirstSecond() <= 2 then
-						self:effectcolor1(color("#FF000080")):effectclock(effectclock):effectcolor2(color("#000000"))
-					else
-						self:effectcolor1(color("#000000")):effectclock(effectclock):effectcolor2(color("#000000"))
-					end
-				end
-			end,
-			CurrentSongChangedMessageCommand=function(self) self:playcommand("Blink") end
 		}
 	}
 }
