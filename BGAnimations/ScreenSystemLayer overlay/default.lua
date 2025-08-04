d5e045be64fa7610cf02a830b160cd6f8251eefe
DarkBahamut162 <dark_bahamut162@hotmail.com>
2025-08-04 18:52:23 +0200

    I need to do this myself since Etterna doesn't have this...

---
+++ BGAnimations/ScreenSystemLayer overlay/default.lua
@@ -0,0 +1,54 @@
+local function CreditsText(pn)
+	local text = LoadFont(Var "LoadingScreen","credits")..{
+		InitCommand=function(self)
+			self:name("Credits"..PlayerNumberToString(pn))
+			ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen")
+		end,
+		UpdateTextCommand=function(self)
+			local str = ScreenSystemLayerHelpers.GetCreditsMessage(pn)
+			self:settext(str)
+		end,
+		UpdateVisibleCommand=function(self)
+			local screen = SCREENMAN:GetTopScreen()
+			local bShow = true
+			if screen then
+				local sClass = screen:GetName()
+				bShow = THEME:GetMetric(sClass,"ShowCreditDisplay")
+			end
+
+			self:visible(bShow)
+		end
+	}
+	return text
+end
+
+local t = Def.ActorFrame{}
+
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenSystemLayer",isEtterna() and "error" or "aux"))
+t[#t+1] = Def.ActorFrame{
+	CreditsText(PLAYER_1),
+	CreditsText(PLAYER_2)
+}
+t[#t+1] = Def.ActorFrame{
+	Def.Quad{
+		InitCommand=function(self) self:zoomtowidth(SCREEN_WIDTH):zoomtoheight(30):horizalign(left):vertalign(top):y(SCREEN_TOP):diffuse(color("0,0,0,0")) end,
+		OnCommand=function(self) self:finishtweening():diffusealpha(0.85) end,
+		OffCommand=function(self) self:sleep(3):linear(0.5):diffusealpha(0) end
+	},
+	Def.BitmapText{
+		Font="Common Normal",
+		Name="Text",
+		InitCommand=function(self) self:maxwidth(750):horizalign(left):vertalign(top):y(SCREEN_TOP+10):x(SCREEN_LEFT+10):shadowlength(1):diffusealpha(0) end,
+		OnCommand=function(self) self:finishtweening():diffusealpha(1):zoom(0.5) end,
+		OffCommand=function(self) self:sleep(3):linear(0.5):diffusealpha(0) end
+	},
+	SystemMessageMessageCommand=function(self,params)
+		self:GetChild("Text"):settext(params.Message)
+		self:playcommand("On")
+		if params.NoAnimate then self:finishtweening() end
+		self:playcommand("Off")
+	end,
+	HideSystemMessageMessageCommand=function(self) self:finishtweening() end
+}
+
+return t
