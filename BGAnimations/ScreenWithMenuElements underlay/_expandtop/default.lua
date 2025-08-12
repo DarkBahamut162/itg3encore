54b05e29470f4de2189f3d6bc1f573f64d92b38c
DarkBahamut162 <dark_bahamut162@hotmail.com>
2025-08-12 10:43:11 +0200

    If there is no timer, don't bother displaying that field for nothing

--- BGAnimations/ScreenWithMenuElements underlay/_expandtop/default.lua
+++ BGAnimations/ScreenWithMenuElements underlay/_expandtop/default.lua
@@ -1,15 +1,19 @@
 return Def.ActorFrame{
 	Def.Sprite {
-		Texture = "_lside",
-		Condition=not isEtterna() and not isFinal(),
-		InitCommand=function(self) self:x(SCREEN_LEFT):zoom(WideScreenDiff()):halign(0):valign(0) end,
+		Texture = "_width "..(isFinal() and "final" or "normal"),
+		InitCommand=function(self) self:x(SCREEN_LEFT):zoom(WideScreenDiff()):valign(0):halign(0):zoomtowidth(SCREEN_WIDTH) end,
 		OnCommand=function(self) self:y(SCREEN_TOP-100):decelerate(0.8):y(SCREEN_TOP) end,
 		OffCommand=function(self) self:accelerate(0.5):addy(-100) end
 	},
 	Def.Sprite {
-		Texture = "_width "..(isFinal() and "final" or "normal"),
-		InitCommand=function(self) self:x((isEtterna() or isFinal()) and SCREEN_LEFT or 180*WideScreenDiff()):zoom(WideScreenDiff()):valign(0):halign(0):zoomtowidth(SCREEN_WIDTH) end,
-		OnCommand=function(self) self:y(SCREEN_TOP-100):decelerate(0.8):y(SCREEN_TOP) end,
+		Texture = "_lside",
+		Condition=not isEtterna() and not isFinal(),
+		InitCommand=function(self) self:x(SCREEN_LEFT):zoom(WideScreenDiff()):halign(0):valign(0) end,
+		OnCommand=function(self)
+			local timer = SCREENMAN:GetTopScreen():GetChild('Timer')
+			if not timer then self:diffusealpha(0) end
+			self:y(SCREEN_TOP-100):decelerate(0.8):y(SCREEN_TOP)
+		end,
 		OffCommand=function(self) self:accelerate(0.5):addy(-100) end
 	},
 	Def.Sprite {
