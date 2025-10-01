014c9ee0a2982baa0f64abff0ef10db896e5462c
DarkBahamut162 <dark_bahamut162@hotmail.com>
2025-10-01 12:57:30 +0200

    Turn Quad into Quint if FAScore is also 100% [Evaluation Only]

--- Graphics/GradeDisplayEval Tier01.lua
+++ Graphics/GradeDisplayEval Tier01.lua
@@ -1,4 +1,21 @@
 return Def.ActorFrame{
+	BeginCommand=function(self)
+		local name = self:GetParent():GetName():sub(-2)
+		if name ~= "" then
+			local faCheck = getenv("SetScoreFA"..name)
+			if faCheck then
+				local W0Count = getenv("W0"..name) or 0
+				local WXCount = getenv("WX"..name) or 0
+				if W0Count > 0 and WXCount == 0 then else
+					self:GetChild("Quint"):diffusealpha(0)
+				end
+			else
+				self:GetChild("Quint"):diffusealpha(0)
+			end
+		else
+			self:GetChild("Quint"):diffusealpha(0)
+		end
+	end,
 	Def.Model{
 		Materials=THEME:GetPathG("_grade","models/star.txt"),
 		Meshes=THEME:GetPathG("_grade","models/star.txt"),
@@ -18,6 +35,19 @@ return Def.ActorFrame{
 	Def.Sprite {
 		Texture = THEME:GetPathG("_grade","models/goldflare"),
 		InitCommand=function(self) self:blend(Blend.Add):x(25):y(-21):zoom(0.45):wag():effectmagnitude(0,4,10) end
+	},
+	Def.ActorFrame{
+		Name="Quint",
+		Def.Model{
+			Materials=THEME:GetPathG("_grade","models/star.txt"),
+			Meshes=THEME:GetPathG("_grade","models/star.txt"),
+			Bones=THEME:GetPathG("_grade","models/star.txt"),
+			InitCommand=function(self) self:zoom(0.5):wag():effectmagnitude(0,4,10) end
+		},
+		Def.Sprite {
+			Texture = THEME:GetPathG("_grade","models/goldflare"),
+			InitCommand=function(self) self:blend(Blend.Add):zoom(0.45):wag():effectmagnitude(0,4,10) end
+		}
 	},
 	Def.Model{
 		Materials=THEME:GetPathG("_grade","models/star.txt"),
