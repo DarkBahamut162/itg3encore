f4a33177694aea25c9db7851b9bb4b03dac5df39
DarkBahamut162 <dark_bahamut162@hotmail.com>
2025-10-28 07:10:49 +0100

    Unclutter core in PlayerScoreDisplay

--- Graphics/_player scores.lua
+++ Graphics/_player scores.lua
@@ -1,57 +1,78 @@
 local player = ...
 assert(player,"[Graphics/_player scores] player required")
 local courseMode = GAMESTATE:IsCourseMode()
+local c
+local enableFlare = GAMESTATE:IsPlayerEnabled(player) and getenv("Flare"..pname(player)) > 0 or false
 
 return Def.ActorFrame{
-	InitCommand=function(self) if IsUsingWideScreen() and hasAvatar(player) then self:x(player == PLAYER_1 and 48 or -48) end end,
-	CurrentSongChangedMessageCommand=function(self) if not courseMode then self:RunCommandsRecursively( function(self) self:queuecommand("Set") end ) end end,
-	CurrentCourseChangedMessageCommand=function(self) if courseMode then self:RunCommandsRecursively( function(self) self:queuecommand("Set") end )  end end,
-	["CurrentSteps".. pname(player) .."ChangedMessageCommand"]=function(self) if not courseMode then self:RunCommandsRecursively( function(self) self:queuecommand("Set") end )  end end,
-	CurrentStepsChangedMessageCommand=function(self) if not courseMode then self:RunCommandsRecursively( function(self) self:queuecommand("Set") end )  end end,
-	["CurrentTrail".. pname(player) .."ChangedMessageCommand"]=function(self) if courseMode then self:RunCommandsRecursively( function(self) self:queuecommand("Set") end )  end end,
+	InitCommand=function(self) c = self:GetChildren() if IsUsingWideScreen() and hasAvatar(player) then self:x(player == PLAYER_1 and 48 or -48) end end,
+	CurrentSongChangedMessageCommand=function(self) if not courseMode then self:queuecommand("Set") end end,
+	CurrentCourseChangedMessageCommand=function(self) if courseMode then self:queuecommand("Set") end end,
+	["CurrentSteps".. pname(player) .."ChangedMessageCommand"]=function(self) if not courseMode then self:queuecommand("Set") end end,
+	CurrentStepsChangedMessageCommand=function(self) if not courseMode then self:queuecommand("Set") end end,
+	["CurrentTrail".. pname(player) .."ChangedMessageCommand"]=function(self) if courseMode then self:queuecommand("Set") end end,
+	SetCommand=function(self)
+		local SongOrCourse = courseMode and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
+		local StepsOrTrail = courseMode and GAMESTATE:GetCurrentTrail(player) or GAMESTATE:GetCurrentSteps(player)
+		local flare = 0
+
+		if SongOrCourse and StepsOrTrail then
+			if isEtterna() then
+				local scores = GetDisplayScore()
+				if scores then
+					c.MachineScore:GetChild("ScoreName"):settext("NEXT")
+					local grade = scores:GetWifeGrade()
+					if grade == "Grade_Failed" then grade = "Grade_Tier16" end
+					c.MachineScore:GetChild("ScorePercent"):settext(FormatPercentScore(GetPercentFromGradeWife(string.format("Grade_Tier%02d",(tonumber(grade:sub(-2))-1)))))
+					c.ProfileScore:GetChild("ScorePercent"):settext( string.format("%0.2f%%",scores:GetWifeScore()*100))
+				else
+					c.MachineScore:GetChild("ScoreName"):settext("NEXT")
+					c.MachineScore:GetChild("ScorePercent"):settext(FormatPercentScore(GetPercentFromGradeWife("Grade_Tier15")))
+					c.ProfileScore:GetChild("ScorePercent"):settext("0.00%")
+				end
+			else
+				local profileMachine = PROFILEMAN:GetMachineProfile()
+				local hsl = profileMachine:GetHighScoreList(SongOrCourse,StepsOrTrail)
+				local scores = hsl and hsl:GetHighScores()
+				if scores[1] then
+					c.MachineScore:GetChild("ScoreName"):settext(scores[1]:GetName())
+					c.MachineScore:GetChild("ScorePercent"):settext(string.format("%0.2f%%",scores[1]:GetPercentDP()*100))
+				else
+					c.MachineScore:GetChild("ScoreName"):settext("N/A")
+					c.MachineScore:GetChild("ScorePercent"):settext("0.00%")
+				end
+
+				local profilePlayer = PROFILEMAN:GetProfile(player)
+				local hsl = profilePlayer:GetHighScoreList(SongOrCourse,StepsOrTrail)
+				local scores = hsl and hsl:GetHighScores()
+				if scores[1] then
+					c.ProfileScore:GetChild("ScorePercent"):settext(string.format("%0.2f%%",scores[1]:GetPercentDP()*100))
+				else
+					c.ProfileScore:GetChild("ScorePercent"):settext("0.00%")
+				end
+			end
+			if enableFlare then flare = GetFlare(player,SongOrCourse,StepsOrTrail) end
+		else
+			c.MachineScore:GetChild("ScoreName"):settext("?")
+			c.MachineScore:GetChild("ScorePercent"):settext("?")
+			c.ProfileScore:GetChild("ScorePercent"):settext("?")
+		end
+		if enableFlare then
+			if flare == 10 then
+				c.ProfileScore:GetChild("ScoreName"):rainbow()
+				c.ProfileScore:GetChild("FlareName"):settext("FX")
+			elseif flare == 0 then
+				c.ProfileScore:GetChild("ScoreName"):stopeffect():diffuse(color("#fff"))
+				c.ProfileScore:GetChild("FlareName"):settext("")
+			else
+				c.ProfileScore:GetChild("ScoreName"):stopeffect():diffuse(color(flareColor[flare]))
+				c.ProfileScore:GetChild("FlareName"):settext("F"..flare)
+			end
+		end
+	end,
 	Def.ActorFrame{
 		Name="MachineScore",
 		InitCommand=function(self) self:x(-55) end,
-		SetCommand=function(self)
-			local name = self:GetChild("ScoreName")
-			local score = self:GetChild("ScorePercent")
-			local profile = PROFILEMAN:GetMachineProfile()
-			local SongOrCourse = courseMode and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
-			local StepsOrTrail = courseMode and GAMESTATE:GetCurrentTrail(player) or GAMESTATE:GetCurrentSteps(player)
-			local scoreText = "?"
-			local nameText = "?"
-
-			if SongOrCourse and StepsOrTrail then
-				if isEtterna() then
-					local scores = GetDisplayScore()
-					if scores then
-						nameText = "NEXT"
-						local grade = scores:GetWifeGrade()
-						if grade == "Grade_Failed" then grade = "Grade_Tier16" end
-						scoreText = FormatPercentScore(GetPercentFromGradeWife(string.format("Grade_Tier%02d",(tonumber(grade:sub(-2))-1))))
-					else
-						nameText = "NEXT"
-						scoreText = FormatPercentScore(GetPercentFromGradeWife("Grade_Tier15"))
-					end
-				else
-					local hsl = profile:GetHighScoreList(SongOrCourse,StepsOrTrail)
-					local scores = hsl and hsl:GetHighScores()
-					if scores[1] then
-						nameText = scores[1]:GetName()
-						scoreText = string.format("%0.2f%%",scores[1]:GetPercentDP()*100)
-					else
-						nameText = "N/A"
-						scoreText = "0.00%"
-					end
-				end
-			else
-				nameText = "?"
-				scoreText = "?"
-			end
-
-			name:settext(nameText)
-			score:settext(scoreText)
-		end,
 		Def.BitmapText {
 			File = "_z 36px shadowx",
 			Name="ScoreName",
@@ -73,77 +94,19 @@ return Def.ActorFrame{
 	Def.ActorFrame{
 		Name="ProfileScore",
 		InitCommand=function(self) self:x(46) end,
-		SetCommand=function(self)
-			local name = self:GetChild("ScoreName")
-			local score = self:GetChild("ScorePercent")
-			local f = self:GetChild("FlareName")
-			local profile = PROFILEMAN:GetProfile(player)
-			local SongOrCourse = courseMode and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
-			local StepsOrTrail = courseMode and GAMESTATE:GetCurrentTrail(player) or GAMESTATE:GetCurrentSteps(player)
-			local scoreText = "?"
-			local nameText = "YOU"
-			local flare = 0
-
-			if SongOrCourse and StepsOrTrail then
-				if isEtterna() then
-					local scores = GetDisplayScore()
-					if scores then
-						scoreText = string.format("%0.2f%%",scores:GetWifeScore()*100)
-					else
-						scoreText = "0.00%"
-					end
-				else
-					local hsl = profile:GetHighScoreList(SongOrCourse,StepsOrTrail)
-					local scores = hsl and hsl:GetHighScores()
-					if scores[1] then
-						scoreText = string.format("%0.2f%%",scores[1]:GetPercentDP()*100)
-					else
-						scoreText = "0.00%"
-					end
-				end
-				flare = GetFlare(player,SongOrCourse,StepsOrTrail)
-			end
-			name:settext(nameText)
-			score:settext(scoreText)
-			if flare == 10 then
-				name:rainbow()
-				f:settext("FX")
-			elseif flare == 0 then
-				name:stopeffect():diffuse(color("#fff"))
-				f:settext("")
-			else
-				name:stopeffect():diffuse(color(flareColor[flare]))
-				f:settext("F"..flare)
-			end
-		end,
 		Def.BitmapText {
 			File = "_z 36px shadowx",
 			Name="FlareName",
-			Text="FX",
 			InitCommand=function(self) self:x(32):y(85):rotationz(90):diffusealpha(0):zoom(0.25):shadowlength(2):maxwidth(175) end,
 			OnCommand=function(self) self:sleep(0.85):linear(0.2):diffusealpha(1) end,
-			OffCommand=function(self) self:linear(0.4):diffusealpha(0) end,
-			SetCommand=function(self)
-				local SongOrCourse = courseMode and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
-				local StepsOrTrail = courseMode and GAMESTATE:GetCurrentTrail(player) or GAMESTATE:GetCurrentSteps(player)
-				local flare = 0
-
-				if SongOrCourse and StepsOrTrail then flare = GetFlare(player,SongOrCourse,StepsOrTrail) end
-				if flare == 10 then
-					self:settext("FX")
-				elseif flare == 0 then
-					self:settext("")
-				else
-					self:settext("F"..flare)
-				end
-			end
+			OffCommand=function(self) self:linear(0.4):diffusealpha(0) end
 		},
 		Def.BitmapText {
 			File = "_z 36px shadowx",
 			Name="ScoreName",
-			Text="CARD",
+			Text=HumanAndUSBReady(player) and "CARD" or "YOU",
 			InitCommand=function(self) self:y(85):diffusealpha(0):zoom(0.55):shadowlength(2):maxwidth(175) end,
-			OnCommand=function(self) self:sleep(0.85):linear(0.2):diffusealpha(1) end,
+			OnCommand=function(self) self:sleep(0.85):linear(0.2):diffusealpha(1):diffuse(color("#000")) end,
 			OffCommand=function(self) self:linear(0.4):diffusealpha(0) end
 		},
 		Def.BitmapText {
