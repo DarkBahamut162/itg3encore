f11af1e869c2377ae808f0f3b975ee11e04c341c
DarkBahamut162 <dark_bahamut162@hotmail.com>
2025-11-03 07:42:32 +0100

    Have BattleMeter in Versus [Non-Battle Mode]

--- BGAnimations/ScreenGameplay underlay/stepstats/versus/default.lua
+++ BGAnimations/ScreenGameplay underlay/stepstats/versus/default.lua
@@ -29,212 +29,35 @@ if getenv("ShowStatsP1") == nil or getenv("ShowStatsP2") == nil then return Def.
 			}
 		end
 	end
+	function CompareScoresRange( difference, range )
+		local Player1Score=DP( PLAYER_1 )
+		local Player2Score=DP( PLAYER_2 )
+		local ScoreDifference = Player1Score - Player2Score
+		local ReturnValue = scale(ScoreDifference, -difference, difference, range, -range)
 
-	if not isVS() and not IIDX then
-		local Judgments = Def.ActorFrame{
-			InitCommand=function(self) self:Center() end
-		}
-		local diffuses = {color("#60B5C7"),color("#FEA859"),color("#4AB812"),color("#D064FB"),color("#F76D47"),color("#FB0808")}
-		local texts = {"F","E","G","D","W","M"}
-		for i = 1,math.min(6,math.max(bgNum[PLAYER_1],bgNum[PLAYER_2])) do
-			Judgments[#Judgments+1] = Def.BitmapText {
-				File = "ScreenGameplay judgment",
-				InitCommand=function(self) self:shadowlength(1):y(SCREEN_TOP+(85+13*(i-1))*WideScreenDiff()-SCREEN_HEIGHT/2):zoom(0.5*WideScreenDiff()):diffuse(diffuses[i]):settext(texts[i]) end
-			}
-		end
+		if ReturnValue <= -range then return -range
+		elseif ReturnValue >= range then return range
+		else return ReturnValue end
+	end
 
+	if not isVS() then
 		t[#t+1] = Def.ActorFrame{
-			Name="Labels",
-			InitCommand=function(self) self:y(isVS() and -10*WideScreenDiff() or 0):diffusealpha(0) end,
-			Condition=bgNum[PLAYER_1] > 0 or bgNum[PLAYER_2] > 0,
-			OnCommand=function(self) self:sleep(0.5):decelerate(0.8):diffusealpha(1) end,
-			OffCommand=function(self) if AnyPlayerFullComboed() then self:sleep(1) end self:accelerate(0.8):diffusealpha(0) end,
-			Judgments
-		}
-
-		for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
-			local Numbers = Def.ActorFrame{
-				InitCommand=function(self) self:Center() end
-			}
-			for i = 1,math.min(6,math.max(bgNum[PLAYER_1],bgNum[PLAYER_2])) do
-				local score = i < 6 and "W"..i or "Miss"
-				Numbers[#Numbers+1] = Def.BitmapText {
-					File = "_z numbers",
-					InitCommand=function(self) self:x(pn == PLAYER_1 and -8*WideScreenDiff() or 8*WideScreenDiff()):y(SCREEN_TOP+(85+13*(i-1))*WideScreenDiff()-SCREEN_HEIGHT/2)
-						:zoom(0.6*WideScreenDiff()):maxwidth(125):horizalign(pn == PLAYER_1 and right or left):queuecommand("Update") end,
-					Condition=bgNum[PLAYER_1] >= i or bgNum[PLAYER_2] >= i,
-					UpdateCommand=function(self)
-						self:diffuse(PlayerColor(pn))
-						if bgNum[pn] < i and bgNum[pn == PLAYER_1 and PLAYER_2 or PLAYER_1] >= i then
-							self:settext("?")
-						else
-							local Notes = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):GetTapNoteScores('TapNoteScore_'..score)
-							self:settext(Notes)
-						end
-					end
-				}
-			end
-
-			t[#t+1] = Def.ActorFrame{
-				Name="Numbers"..pname(pn),
-				InitCommand=function(self) self:y(isVS() and -10 or 0):diffusealpha(0) end,
-				Condition=bgNum[PLAYER_1] > 0 or bgNum[PLAYER_2] > 0,
-				OnCommand=function(self) self:sleep(0.5):decelerate(0.8):diffusealpha(1) end,
-				OffCommand=function(self) if AnyPlayerFullComboed() then self:sleep(1) end self:accelerate(0.8):diffusealpha(0) end,
-				JudgmentMessageCommand=function(self,param) if param.Player == pn then self:RunCommandsOnChildren(function(self) self:queuecommand("Update") end) end end,
-				Numbers
-			}
-		end
-	elseif not isVS() and IIDX then
-		local Judgments = Def.ActorFrame{
-			InitCommand=function(self) self:Center() end
-		}
-		local diffuses = atLeastOneHighscoreExists and {color("#16AFF3"),color("#00000000"),color("#00000000"),color("#09FF10"),color("#EA3548")} or {color("#16AFF3"),color("#00000000"),color("#EA3548")}
-		local texts = atLeastOneHighscoreExists and {"P","","","H","T"} or {"P","","T"}
-		for i = 1,#texts do
-			Judgments[#Judgments+1] = Def.BitmapText {
-				File = "ScreenGameplay judgment",
-				InitCommand=function(self) self:shadowlength(1):y(SCREEN_TOP+(85+16*(i-1))*WideScreenDiff()-SCREEN_HEIGHT/2):zoom(0.5*WideScreenDiff()):diffuse(diffuses[i]):settext(texts[i]) end
-			}
-		end
-
-		t[#t+1] = Def.ActorFrame{
-			Name="Labels",
-			InitCommand=function(self) self:y(isVS() and -10*WideScreenDiff() or 0):diffusealpha(0) end,
-			Condition=bgNum[PLAYER_1] > 0 or bgNum[PLAYER_2] > 0,
-			OnCommand=function(self) self:sleep(0.5):decelerate(0.8):diffusealpha(1) end,
-			OffCommand=function(self) if AnyPlayerFullComboed() then self:sleep(1) end self:accelerate(0.8):diffusealpha(0) end,
-			Judgments
-		}
-
-		t[#t+1] = Def.ActorFrame{
-			InitCommand=function(self) self:CenterX() end,
-			Def.ActorFrame{
-				InitCommand=function(self) self:y(SCREEN_TOP+85*WideScreenDiff()) end,
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="PlayerPointsP1",
-					Text="0",
-					OnCommand=function(self)
-						self:maxwidth(100):horizalign(right):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(16*0*WideScreenDiff()):addx(-5*WideScreenDiff()):diffuse(PlayerColor(PLAYER_1))
-						:settext(STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1):GetActualDancePoints())
-					end,
-					JudgmentMessageCommand=function(self,param) if param.Player == PLAYER_1 then self:queuecommand("Update") end end,
-					UpdateCommand=function(self) self:settext(STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1):GetActualDancePoints()) end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Condition=topscore[PLAYER_1] ~= nil,
-					Name="HighscorePointsP1",
-					OnCommand=function(self)
-						self:maxwidth(100):horizalign(right):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(16*3*WideScreenDiff()):addx(-5*WideScreenDiff()):diffuse(PlayerColor(PLAYER_1)):settext(math.ceil(topscore[PLAYER_1]:GetPercentDP()*STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1):GetPossibleDancePoints()))
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="TargetPointsP1",
-					Text="0",
-					OnCommand=function(self)
-						local target = THEME:GetMetric("PlayerStageStats", "GradePercentTier" .. string.format("%02d", 18-getenv("SetPacemaker"..pname(PLAYER_1))))
-						self:maxwidth(100):horizalign(right):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(atLeastOneHighscoreExists and 16*4*WideScreenDiff() or 16*2*WideScreenDiff()):addx(-5*WideScreenDiff()):diffuse(PlayerColor(PLAYER_1))
-						:settext(math.ceil(target*STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1):GetPossibleDancePoints()))
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="PlayerPointsP2",
-					Text="0",
-					OnCommand=function(self)
-						self:maxwidth(100):horizalign(left):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(16*0*WideScreenDiff()):addx(5*WideScreenDiff()):diffuse(PlayerColor(PLAYER_2))
-						:settext(STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2):GetActualDancePoints())
-					end,
-					JudgmentMessageCommand=function(self,param) if param.Player == PLAYER_2 then self:queuecommand("Update") end end,
-					UpdateCommand=function(self) self:settext(STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2):GetActualDancePoints()) end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Condition=topscore[PLAYER_2] ~= nil,
-					Name="HighscorePointsP2",
-					OnCommand=function(self)
-						self:maxwidth(100):horizalign(left):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(16*3*WideScreenDiff()):addx(5*WideScreenDiff()):diffuse(PlayerColor(PLAYER_2)):settext(math.ceil(topscore[PLAYER_2]:GetPercentDP()*STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2):GetPossibleDancePoints()))
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="TargetPointsP2",
-					Text="0",
-					OnCommand=function(self)
-						local target = THEME:GetMetric("PlayerStageStats", "GradePercentTier" .. string.format("%02d", 18-getenv("SetPacemaker"..pname(PLAYER_2))))
-						self:maxwidth(100):horizalign(left):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(atLeastOneHighscoreExists and 16*4*WideScreenDiff() or 16*2*WideScreenDiff()):addx(5*WideScreenDiff()):diffuse(PlayerColor(PLAYER_2))
-						:settext(math.ceil(target*STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2):GetPossibleDancePoints()))
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="PlayerHighscoreDifferenceP1",
-					OnCommand=function(self)
-						self:diffuse(color("#00FF00")):maxwidth(100):horizalign(right):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(16*1*WideScreenDiff()):addx(-5*WideScreenDiff())
-						if topscore[PLAYER_1] ~= nil then self:settextf( "%+04d", 0 ) end
-					end,
-					JudgmentMessageCommand=function(self,param) if param.Player == PLAYER_1 and topscore[PLAYER_1] ~= nil then self:queuecommand("Update") end end,
-					UpdateCommand=function(self)
-						local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1)
-						local DPCurMax = pss:GetCurrentPossibleDancePoints()
-						local curPlayerDP = pss:GetActualDancePoints()
-						local curHighscoreDP = math.ceil(DPCurMax*topscore[PLAYER_1]:GetPercentDP())
-						self:settextf( "%+04d", (curPlayerDP-curHighscoreDP) )
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="PlayerTargetDifferenceP1",
-					OnCommand=function(self)
-						self:diffuse(color("#FF0000")):maxwidth(100):horizalign(right):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(atLeastOneHighscoreExists and 16*2*WideScreenDiff() or 16*1*WideScreenDiff()):addx(-5*WideScreenDiff()):settextf( "%+04d", 0 )
-					end,
-					JudgmentMessageCommand=function(self,param) if param.Player == PLAYER_1 then self:queuecommand("Update") end end,
-					UpdateCommand=function(self)
-						local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1)
-						local DPCurMax = pss:GetCurrentPossibleDancePoints()
-						local curPlayerDP = pss:GetActualDancePoints()
-						local target = THEME:GetMetric("PlayerStageStats", "GradePercentTier" .. string.format("%02d", 18-getenv("SetPacemaker"..pname(PLAYER_1))))
-						local curTargetDP = math.ceil(DPCurMax*target)
-						self:settextf( "%+04d", (curPlayerDP-curTargetDP) )
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="PlayerHighscoreDifferenceP2",
-					OnCommand=function(self)
-						self:diffuse(color("#00FF00")):maxwidth(100):horizalign(left):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(16*1*WideScreenDiff()):addx(5*WideScreenDiff())
-						if topscore[PLAYER_2] ~= nil then self:settextf( "%+04d", 0 ) end
-					end,
-					JudgmentMessageCommand=function(self,param) if param.Player == PLAYER_2 and topscore[PLAYER_2] ~= nil then self:queuecommand("Update") end end,
-					UpdateCommand=function(self)
-						local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2)
-						local DPCurMax = pss:GetCurrentPossibleDancePoints()
-						local curPlayerDP = pss:GetActualDancePoints()
-						local curHighscoreDP = math.ceil(DPCurMax*topscore[PLAYER_2]:GetPercentDP())
-						self:settextf( "%+04d", (curPlayerDP-curHighscoreDP) )
-					end
-				},
-				Def.BitmapText {
-					File = "_z numbers",
-					Name="PlayerTargetDifferenceP2",
-					OnCommand=function(self)
-						self:diffuse(color("#FF0000")):maxwidth(100):horizalign(left):zoom(0.75*WideScreenDiff()):shadowlength(0):addy(atLeastOneHighscoreExists and 16*2*WideScreenDiff() or 16*1*WideScreenDiff()):addx(5*WideScreenDiff()):settextf( "%+04d", 0 )
-					end,
-					JudgmentMessageCommand=function(self,param) if param.Player == PLAYER_2 then self:queuecommand("Update") end end,
-					UpdateCommand=function(self)
-						local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2)
-						local DPCurMax = pss:GetCurrentPossibleDancePoints()
-						local curPlayerDP = pss:GetActualDancePoints()
-						local target = THEME:GetMetric("PlayerStageStats", "GradePercentTier" .. string.format("%02d", 18-getenv("SetPacemaker"..pname(PLAYER_2))))
-						local curTargetDP = math.ceil(DPCurMax*target)
-						self:settextf( "%+04d", (curPlayerDP-curTargetDP) )
-					end
-				}
-			}
+			InitCommand=function(self) self:CenterX():y(SCREEN_TOP+115) end,
+			Def.Sprite {
+				Texture = "bg",
+				InitCommand=function(self) self:animate(0) end,
+				JudgmentMessageCommand=function(self) self:queuecommand("Update") end,
+				UpdateCommand=function(self) self:setstate(CompareScoresRange(0.05,80) < 0 and 1 or 2) end
+			},
+			Def.Sprite {
+				Texture = "bar",
+				InitCommand=function(self) self:y(10) end,
+				JudgmentMessageCommand=function(self) self:queuecommand("Update") end,
+				UpdateCommand=function(self) self:stoptweening():linear(0.1):rotationz(CompareScoresRange(0.05,80)) end
+			},
+			Def.Sprite { Texture = "frame" }
 		}
 	end
+
 	return t
 end
