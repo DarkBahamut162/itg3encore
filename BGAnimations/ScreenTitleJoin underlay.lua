77d841c2d904b7b0261c07a21412b86b67902fa4
DarkBahamut162 <dark_bahamut162@hotmail.com>
2025-12-17 10:32:55 +0100

    Update ScreenTitleJoin in PayMode

--- BGAnimations/ScreenTitleJoin underlay.lua
+++ BGAnimations/ScreenTitleJoin underlay.lua
@@ -4,7 +4,22 @@ return Def.ActorFrame{
 		Condition=isScreenTitle(),
 		Def.BitmapText {
 			File = "_r bold shadow 30px",
-			InitCommand=function(self) self:CenterX():y(SCREEN_BOTTOM-85-25*WideScreenDiff()):shadowlength(0):zoomx(0.8*WideScreenDiff()):zoomy(0.7*WideScreenDiff()):settext("PRESS") end
+			InitCommand=function(self) self:CenterX():y(SCREEN_BOTTOM-85-25*WideScreenDiff()):shadowlength(0):zoomx(0.8*WideScreenDiff()):zoomy(0.7*WideScreenDiff()):playcommand("Refresh") end,
+			RefreshCommand=function(self)
+				if GAMESTATE:GetCoinMode()=='CoinMode_Pay' then
+					local coins=GAMESTATE:GetCoins()
+					local coinsPerCredit=PREFSMAN:GetPreference('CoinsPerCredit')
+					if coins >= coinsPerCredit then
+						self:settext("PRESS START")
+					else
+						self:settext("INSERT COIN")
+					end
+				else
+					self:settext("PRESS START")
+				end
+			end,
+			CoinModeChangedMessageCommand=function(self) self:playcommand("Refresh") end,
+			CoinInsertedMessageCommand=function(self) self:playcommand("Refresh") end
 		},
 		Def.ActorFrame{
 			Name="StartButton",
