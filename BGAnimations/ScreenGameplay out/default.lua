cab676d4e9e8aa44002ae8146dde4e6f7c564d3e
DarkBahamut162 <dark_bahamut162@hotmail.com>
2026-06-04 09:15:07 +0200

    Fix ForceFail

--- BGAnimations/ScreenGameplay out/default.lua
+++ BGAnimations/ScreenGameplay out/default.lua
@@ -41,7 +41,7 @@ return Def.ActorFrame{
 		Def.Sprite {
 			Texture = THEME:GetPathB("ScreenGameplay","failed/_stage "..(isFinal() and "final" or "normal")),
 			InitCommand=function(self) self:CenterX():y(SCREEN_CENTER_Y-75*WideScreenDiff()):zoom(0.6*WideScreenDiff()):diffusealpha(0):addy(-30) end,
-			ForceFailMessageCommand=function(self) self:sleep(0.1):linear(0.3):diffusealpha(1):addy(30) end
+			ForceFailMessageCommand=function(self) self:stoptweening():sleep(0.1):linear(0.3):diffusealpha(1):addy(30) end
 		},
 		Def.ActorFrame{
 			Name="NormalFail",
@@ -49,7 +49,7 @@ return Def.ActorFrame{
 			Def.Sprite {
 				Texture = THEME:GetPathB("ScreenGameplay","failed/_failed "..(isFinal() and "final" or "normal")),
 				InitCommand=function(self) self:zoom(WideScreenDiff()):x(SCREEN_CENTER_X+2*WideScreenDiff()):y(SCREEN_CENTER_Y+30*WideScreenDiff()):diffusealpha(0):addx(-500) end,
-				ForceFailMessageCommand=function(self) self:sleep(0.4):decelerate(0.7):addx(500):diffusealpha(1) end
+				ForceFailMessageCommand=function(self) self:stoptweening():sleep(0.4):decelerate(0.7):addx(500):diffusealpha(1) end
 			}
 		},
 		Def.ActorFrame{
@@ -58,26 +58,30 @@ return Def.ActorFrame{
 			Def.Sprite {
 				Texture = THEME:GetPathB("ScreenGameplay","failed/v_failed"),
 				InitCommand=function(self) self:zoom(WideScreenDiff()):x(SCREEN_CENTER_X+2*WideScreenDiff()):y(SCREEN_CENTER_Y+52*WideScreenDiff()):diffusealpha(0.2):cropleft(0.5):cropright(0.5) end,
-				ForceFailMessageCommand=function(self) self:sleep(0.7):decelerate(0.75):cropright(0):cropleft(0):diffusealpha(1) end
+				ForceFailMessageCommand=function(self) self:stoptweening():sleep(0.7):decelerate(0.75):cropright(0):cropleft(0):diffusealpha(1) end
 			},
 			Def.Sprite {
 				Texture = THEME:GetPathG("horiz-line","short"),
 				InitCommand=function(self) self:zoomx(2*WideScreenDiff()):rotationz(90):x(SCREEN_CENTER_X+1*WideScreenDiff()):y(SCREEN_CENTER_Y+52*WideScreenDiff()):cropleft(0.5):cropright(0.5) end,
-				ForceFailMessageCommand=function(self) self:sleep(0.55):accelerate(0.15):cropleft(0):cropright(0):decelerate(0.725):addx(-SCREEN_WIDTH/2):linear(0.15):diffusealpha(0) end
+				ForceFailMessageCommand=function(self) self:stoptweening():sleep(0.55):accelerate(0.15):cropleft(0):cropright(0):decelerate(0.725):addx(-SCREEN_WIDTH/2):linear(0.15):diffusealpha(0) end
 			},
 			Def.Sprite {
 				Texture = THEME:GetPathG("horiz-line","short"),
 				InitCommand=function(self) self:zoomx(2*WideScreenDiff()):rotationz(90):x(SCREEN_CENTER_X+2*WideScreenDiff()):y(SCREEN_CENTER_Y+52*WideScreenDiff()):cropleft(0.5):cropright(0.5) end,
-				ForceFailMessageCommand=function(self) self:sleep(0.55):accelerate(0.15):cropleft(0):cropright(0):decelerate(0.725):addx(SCREEN_WIDTH/2):linear(0.15):diffusealpha(0) end
+				ForceFailMessageCommand=function(self) self:stoptweening():sleep(0.55):accelerate(0.15):cropleft(0):cropright(0):decelerate(0.725):addx(SCREEN_WIDTH/2):linear(0.15):diffusealpha(0) end
 			}
 		},
 		Def.Quad{
 			InitCommand=function(self) self:FullScreen():diffuse(color("#00000000")) end,
-			ForceFailMessageCommand=function(self) self:sleep(3):linear(0.3):diffusealpha(1) end
+			ForceFailMessageCommand=function(self) self:stoptweening():sleep(3):linear(0.3):diffusealpha(1) end
 		},
 		Def.Sound {
 			File = THEME:GetPathB("ScreenGameplay","failed/bleh.ogg"),
-			ForceFailMessageCommand=function(self) self:play() end
+			ForceFailMessageCommand=function(self)
+				self:play()
+				if isEtterna("0.71") then passed = not STATSMAN:GetCurStageStats():Failed() else passed = STATSMAN:GetCurStageStats():OnePassed() end
+				if passed then self:pause() end
+			end
 		}
 	}
 }
